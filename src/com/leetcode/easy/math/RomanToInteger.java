be6e1dea6cdf5fe7efc34fa3d9fb0001520ac0ea
Abhijeet Muneshwar <openingknots@gmail.com>
2022-08-27 16:57:28 +0530

    🎨 Easy: Roman To Integer

---
+++ src/com/leetcode/easy/math/RomanToInteger.java
@@ -0,0 +1,42 @@
+package com.leetcode.easy.math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created using IntelliJ IDEA. Author:  abhijeet, Date:    27/08/22, Time:    3:27 PM
+ */
+public class RomanToInteger {
+
+  public static void main(String[] args) {
+    RomanToInteger romanToInteger = new RomanToInteger();
+    System.out.println(romanToInteger.romanToInteger("MCMXCIV"));
+    System.out.println(romanToInteger.romanToInteger("LVIII"));
+    System.out.println(romanToInteger.romanToInteger("III"));
+  }
+
+  private int romanToInteger(String romanNo) {
+    Map<Character, Integer> romanToIntMap = new HashMap<>();
+    romanToIntMap.put('I', 1);
+    romanToIntMap.put('V', 5);
+    romanToIntMap.put('X', 10);
+    romanToIntMap.put('L', 50);
+    romanToIntMap.put('C', 100);
+    romanToIntMap.put('D', 500);
+    romanToIntMap.put('M', 1000);
+    int len = romanNo.length() - 1;
+    char currentChar = romanNo.charAt(len);
+    int intNo = romanToIntMap.get(currentChar);
+    char previousChar = currentChar;
+    while (--len >= 0) {
+      currentChar = romanNo.charAt(len);
+      if (romanToIntMap.get(currentChar) < romanToIntMap.get(previousChar)) {
+        intNo -= romanToIntMap.get(currentChar);
+      } else {
+        intNo += romanToIntMap.get(currentChar);
+      }
+      previousChar = currentChar;
+    }
+    return intNo;
+  }
+}
