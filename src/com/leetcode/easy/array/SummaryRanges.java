062340b464e9f6b920e58349c111ebd11038c797
Abhijeet Muneshwar <openingknots@gmail.com>
2022-09-10 16:30:32 +0530

    🎨 Easy : Array: Summary Ranges

--- src/com/leetcode/easy/array/SummaryRanges.java
+++ src/com/leetcode/easy/array/SummaryRanges.java
@@ -11,12 +11,26 @@ public class SummaryRanges {
 
   public static void main(String[] args) {
     SummaryRanges summaryRanges = new SummaryRanges();
-    int[] nums = new int[]{0, 1, 2, 4, 5, 7};
+    //int[] nums = new int[]{0, 1, 2, 4, 5, 7};
+    int[] nums = new int[]{0, 2, 3, 4, 6, 8, 9};
     List<String> ranges = summaryRanges.summaryRanges(nums);
+    System.out.println(ranges.toArray());
   }
 
   private List<String> summaryRanges(int[] nums) {
     List<String> ranges = new ArrayList<>();
+    int size = nums.length;
+    for (int i = 0; i < size; i++) {
+      int start = nums[i];
+      while ((i + 1 < size) && (nums[i] + 1 == nums[i + 1])) {
+        i++;
+      }
+      if (start == nums[i]) {
+        ranges.add("" + start);
+      } else {
+        ranges.add(start + "->" + nums[i]);
+      }
+    }
     return ranges;
   }
 }
