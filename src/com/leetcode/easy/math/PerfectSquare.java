b18108fdea4372c455e780a5c26e775c1aa0c6c0
Abhijeet Muneshwar <openingknots@gmail.com>
2022-09-16 09:42:05 +0530

    🎨 Easy: Array, Math

---
+++ src/com/leetcode/easy/math/PerfectSquare.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy.math;
+
+/**
+ * Created using IntelliJ IDEA. Author:  abhijeet, Date:    16/09/22, Time:    9:40 AM
+ * https://leetcode.com/problems/valid-perfect-square/
+ */
+public class PerfectSquare {
+
+  public static void main(String[] args) {
+    PerfectSquare perfectSquare = new PerfectSquare();
+    System.out.println(perfectSquare.isPerfectSquare(14));
+  }
+
+  private boolean isPerfectSquare(int num) {
+    
+    return false;
+  }
+}
