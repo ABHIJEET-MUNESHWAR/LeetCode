63ab416e7952a1d1e2dc8f05aab5cd8005e98d7d
Abhijeet Muneshwar <openingknots@gmail.com>
2022-09-16 10:39:31 +0530

    🎨 Easy: Perfect Square

--- src/com/leetcode/easy/math/PerfectSquare.java
+++ src/com/leetcode/easy/math/PerfectSquare.java
@@ -8,11 +8,22 @@ public class PerfectSquare {
 
   public static void main(String[] args) {
     PerfectSquare perfectSquare = new PerfectSquare();
-    System.out.println(perfectSquare.isPerfectSquare(14));
+    System.out.println(perfectSquare.isPerfectSquare(1));
   }
 
   private boolean isPerfectSquare(int num) {
-    
+    int left = 1;
+    int right = num;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (mid < num / mid) {
+        left = mid + 1;
+      } else if (mid > num / mid) {
+        right = mid - 1;
+      } else {
+        return num % mid == 0;
+      }
+    }
     return false;
   }
 }
