e7626d4c66fb74e96db554ae90a35961e138d0ba
Abhijeet Muneshwar <openingknots@gmail.com>
2022-11-24 22:39:23 +0530

    ✨ Easy: Array: Find Relative Ranks

---
+++ src/com/leetcode/utils/ArrayUtils.java
@@ -0,0 +1,21 @@
+package com.leetcode.utils;
+
+/**
+ * Created using IntelliJ IDEA. Author:  abhijeet, Date:    24/11/22, Time:    9:31 PM
+ */
+public class ArrayUtils {
+
+  public static void printArray(String[] arr) {
+    int size = arr.length;
+    for (int i = 0; i < size; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void printArray(Integer[] arr) {
+    int size = arr.length;
+    for (int i = 0; i < size; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
