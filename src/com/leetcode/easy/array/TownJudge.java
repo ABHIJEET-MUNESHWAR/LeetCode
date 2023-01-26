6c09763c2afb881c1fd50f80b5811fa11cfab292
Abhijeet Muneshwar <openingknots@gmail.com>
2023-01-26 12:56:08 +0530

    🎨 Easy: Array: Find town judge

--- src/com/leetcode/easy/array/TownJudge.java
+++ src/com/leetcode/easy/array/TownJudge.java
@@ -1,10 +1,8 @@
 package com.leetcode.easy.array;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 /**
  * Created using IntelliJ IDEA. Author:  abhijeet, Date:    23/01/23, Time:    8:41 am
@@ -20,31 +18,43 @@ public class TownJudge {
 //    int n = 3;
 //    int[][] trust = {{1, 3}, {2, 3}, {3, 1}};
 //    int n = 3;
-    int[][] trust = {{1, 2}, {2, 3}};
+    int[][] trust = {{1, 3}, {2, 3}};
     int n = 3;
     System.out.println(townJudge.findJudge(n, trust));
+    System.out.println(townJudge.findTownJudge(n, trust));
+  }
+
+  private int findTownJudge(int n, int[][] trust) {
+    int[] count = new int[n + 1];
+    for (int[] t : trust) {
+      count[t[0]]--;
+      count[t[1]]++;
+    }
+    for (int i = 1; i <= n; i++) {
+      if (count[i] == n - 1) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   private int findJudge(int n, int[][] trust) {
-    int judgeLocation = -1;
-    Set<Integer> visited = new HashSet<>();
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < trust.length; i++) {
-      map.put(trust[i][0], trust[i][1]);
+    if (trust.length == 0 && n == 1) {
+      return 1;
     }
-    for (Entry<Integer, Integer> entry : map.entrySet()) {
+    Map<Integer, Integer> people = new HashMap<>();
+    Map<Integer, Integer> possibleJudges = new HashMap<>();
+    for (int i = 0; i < trust.length; i++) {
+      people.put(trust[i][0], people.getOrDefault(trust[i][0], 0) + 1);
+      possibleJudges.put(trust[i][1], possibleJudges.getOrDefault(trust[i][1], 0) + 1);
+    }
+    for (Entry<Integer, Integer> entry : possibleJudges.entrySet()) {
       int key = entry.getKey();
       int value = entry.getValue();
-      while (map.containsKey(value)) {
-        value = map.get(value);
-        if (!visited.isEmpty() && visited.contains(value)) {
-          return -1;
-        } else {
-          visited.add(value);
-        }
+      if (!people.containsKey(key) && value == n - 1) {
+        return key;
       }
-      judgeLocation = value;
     }
-    return judgeLocation;
+    return -1;
   }
 }
