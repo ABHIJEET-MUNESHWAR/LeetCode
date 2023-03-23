1972b54da4c81e7eca40d218f75674dcc9cd2de7
Abhijeet Muneshwar <openingknots@gmail.com>
2023-03-23 14:22:17 +0530

    🎨 Easy: Array: Sum of odd length sub arrays

---
+++ src/com/leetcode/easy/array/SumOddLengthSubArrays.java
@@ -0,0 +1,31 @@
+package com.leetcode.easy.array;
+
+/**
+ * Created using IntelliJ IDEA. Author:  abhijeet, Date:    23/03/23, Time:    11:02 am
+ * https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+ */
+public class SumOddLengthSubArrays {
+
+  public static void main(String[] args) {
+    SumOddLengthSubArrays sumOddLengthSubArrays = new SumOddLengthSubArrays();
+    int[] arr = new int[]{1, 4, 2, 5, 3};
+    System.out.println(sumOddLengthSubArrays.sumOddLengthSubArrays(arr));
+  }
+
+  private int sumOddLengthSubArrays(int[] arr) {
+    int sum = 0;
+    int size = arr.length;
+    for (int i = 1; i <= size; i += 2) {
+      int startIndex = 0;
+      int endIndex = startIndex + i;
+      while (endIndex <= size) {
+        for (int j = startIndex; j < endIndex; j++) {
+          sum += arr[j];
+        }
+        startIndex++;
+        endIndex++;
+      }
+    }
+    return sum;
+  }
+}
