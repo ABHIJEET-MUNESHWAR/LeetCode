bd6060367b9a8cf58af90229faecbe4dc52e05ba
Abhijeet Muneshwar <openingknots@gmail.com>
2023-09-15 08:48:17 +0530

    🎨 Easy: Tree: Increasing Order Search Tree

---
+++ src/com/leetcode/easy/tree/IncreasingBST.java
@@ -0,0 +1,44 @@
+package com.leetcode.easy.tree;
+
+import com.leetcode.utils.TreeNode;
+
+/**
+ * Created using IntelliJ IDEA.
+ * Author:  abhijeet,
+ * Date:    13/09/23,
+ * Time:    9:49 am
+ * https://leetcode.com/problems/increasing-order-search-tree/
+ */
+public class IncreasingBST {
+  public static void main(String[] args) {
+    IncreasingBST increasingBST = new IncreasingBST();
+    TreeNode root = new TreeNode(5);
+    root.left = new TreeNode(3);
+    root.right = new TreeNode(6);
+    root.left.left = new TreeNode(2);
+    root.left.right = new TreeNode(4);
+    root.left.left.left = new TreeNode(1);
+    root.right.right = new TreeNode(8);
+    root.right.right.left = new TreeNode(7);
+    root.right.right.right = new TreeNode(9);
+    root = increasingBST.increasingBST(root);
+    root.inorderTraversal(root);
+  }
+
+  private TreeNode increasingBST(TreeNode root) {
+    if (root == null) {
+      return root;
+    }
+    return increasingBST(root, null);
+  }
+
+  private TreeNode increasingBST(TreeNode root, TreeNode tail) {
+    if (root == null) {
+      return tail;
+    }
+    TreeNode res = increasingBST(root.left, root);
+    root.left = null;
+    root.right = increasingBST(root.right, tail);
+    return res;
+  }
+}
