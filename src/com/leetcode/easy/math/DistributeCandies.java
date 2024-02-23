866a1ab85e6d558735d88b151805f2ea7efe061e
Abhijeet Muneshwar <openingknots@gmail.com>
2024-02-23 09:48:55 +0530

    🎨 Easy: Math, Array: Distribute candies

--- src/com/leetcode/easy/math/DistributeCandies.java
+++ src/com/leetcode/easy/math/DistributeCandies.java
@@ -15,7 +15,15 @@ public class DistributeCandies {
   }
 
   private int distributeCandies(int n, int limit) {
-    int answer = 0;
-    return answer;
+    int count = 0;
+    for (int i = 0; i <= limit; i++) {
+      for (int j = 0; j <= limit; j++) {
+        int k = n - i - j;
+        if (k >= 0 && k <= limit) {
+          count++;
+        }
+      }
+    }
+    return count;
   }
 }
