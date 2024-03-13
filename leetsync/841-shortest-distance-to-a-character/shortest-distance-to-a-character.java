9b6b9515b75af747a0506a9062c8d7d9cec1c0cd
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-03-13 17:35:46 +0530

    Time: 1 ms (98.67%) | Memory: 42.4 MB (57.93%) - LeetSync

--- leetsync/841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
+++ leetsync/841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
-
         int size = s.length();
         int[] answer = new int[size];
+        int pos = -size;
         for (int i = 0; i < size; i++) {
-            char ch = s.charAt(i);
-            if (ch == c) {
-                answer[i] = 0;
-            } else {
-                int a = i, b = i;
-                while (a >= 0 || b <= size) {
-                    if ((a >= 0) && (s.charAt(a) == c)) {
-                        answer[i] = i - a;
-                        break;
-                    }
-                    if ((b < size) && (s.charAt(b) == c)) {
-                        answer[i] = b - i;
-                        break;
-                    }
-                    a--;
-                    b++;
-                }
+            if (s.charAt(i) == c) {
+                pos = i;
             }
+            answer[i] = i - pos;
+        }
+        for (int i = pos - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
+                pos = i;
+            }
+            answer[i] = Math.min(answer[i], pos - i);
         }
         return answer;
     }
