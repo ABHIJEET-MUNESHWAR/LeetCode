84f8cb03055bac9a6c0f756394c18acbe1a58033
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-03-14 07:54:34 +0530

    Time: 1082 ms (26.99%) | Memory: 45.9 MB (86.59%) - LeetSync

---
+++ leetsync/560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int goal) {
+        int count = 0;
+        int size = nums.length;
+        for (int i = 0; i < size; i++) {
+            int prefixSum = 0;
+            for (int j = i; j < size; j++) {
+                prefixSum += nums[j];
+                if (prefixSum == goal) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
