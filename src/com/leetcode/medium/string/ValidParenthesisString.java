d566e6437eb818ce015b4002b98d71e2c7248fec
Abhijeet Muneshwar <openingknots@gmail.com>
2024-04-19 10:35:07 +0530

    🎨 [Medium] Stack: 678. Valid Parenthesis String

--- src/com/leetcode/medium/string/ValidParenthesisString.java
+++ src/com/leetcode/medium/string/ValidParenthesisString.java
@@ -1,5 +1,7 @@
 package com.leetcode.medium.string;
 
+import java.util.Stack;
+
 /**
  * Created using IntelliJ IDEA.
  * Author:  abhijeet,
@@ -11,5 +13,42 @@ package com.leetcode.medium.string;
 public class ValidParenthesisString {
   public static void main(String[] args) {
     ValidParenthesisString validParenthesisString = new ValidParenthesisString();
+    System.out.println(validParenthesisString.checkValidString(
+        "(((((()*)(*)*))())())(()())())))((**)))))(()())()"));
+  }
+
+  private boolean checkValidString(String s) {
+    Stack<Integer> openingStack = new Stack<>();
+    Stack<Integer> starStack = new Stack<>();
+    int size = s.length();
+    for (int i = 0; i < size; i++) {
+      char ch = s.charAt(i);
+      switch (ch) {
+        case '*':
+          starStack.push(i);
+          break;
+        case '(':
+          openingStack.push(i);
+          break;
+        case ')':
+          if (!openingStack.isEmpty()) {
+            openingStack.pop();
+          } else if (!starStack.isEmpty()) {
+            starStack.pop();
+          } else {
+            return false;
+          }
+          break;
+      }
+    }
+    while (!openingStack.isEmpty()) {
+      if (starStack.isEmpty()) {
+        return false;
+      }
+      if (openingStack.pop() > starStack.pop()) {
+        return false;
+      }
+    }
+    return openingStack.isEmpty();
   }
 }
