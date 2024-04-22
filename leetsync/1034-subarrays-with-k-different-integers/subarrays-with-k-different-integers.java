1b7667583a2886695a1f6d3706e86903f76ad14d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-04-22 21:50:51 +0530

    Time: 48 ms (19.31%) | Memory: 47.4 MB (48.86%) - LeetSync

--- leetsync/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ leetsync/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,19 +1,28 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int res = 0, sz = nums.length;
-        int[] cnt = new int[sz + 1];
-        for (int l = 0, m = 0, r = 0; r < sz; ++r) {
-            if (++cnt[nums[r]] == 1)
-                if (--k < 0) {
-                    cnt[nums[m++]] = 0;
-                    l = m;
+        return countOfSubarraysWithAtMostK(nums, k) - countOfSubarraysWithAtMostK(nums, k - 1);
+    }
+
+    private int countOfSubarraysWithAtMostK(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        int start = 0;
+        int end = 0;
+        int count = 0;
+        int size = nums.length;
+        while (end < size) {
+            // Expansion
+            frequencyMap.put(nums[end], frequencyMap.getOrDefault(nums[end], 0) + 1);
+            // Shrinking (At most K elements are supported)
+            while (frequencyMap.size() > k) {
+                frequencyMap.put(nums[start], frequencyMap.getOrDefault(nums[start], 0) - 1);
+                if (frequencyMap.get(nums[start]) == 0) {
+                    frequencyMap.remove(nums[start]);
                 }
-            if (k <= 0) {
-                while (cnt[nums[m]] > 1)
-                    --cnt[nums[m++]];
-                res += m - l + 1;
+                start++;
             }
+            count += (end - start + 1);
+            end++;
         }
-        return res;
+        return count;
     }
 }
