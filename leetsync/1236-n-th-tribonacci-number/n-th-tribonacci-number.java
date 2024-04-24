b3018bf7411d34348644951130a0bced38a4d907
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-04-24 09:43:17 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (84.21%) - LeetSync

--- leetsync/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ leetsync/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,10 +1,13 @@
 class Solution {
-    public int tribonacci(int n) {
-
-        int[] dp = { 0, 1, 1 };
-        for (int i = 3; i <= n; i++) {
-            dp[i % 3] = dp[0] + dp[1] + dp[2];
+        public int tribonacci(int n) {
+        if (n < 2) return n;
+        int a = 0, b = 1, c = 1, d;
+        while (n-- > 2) {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
         }
-        return dp[n % 3];
+        return c;
     }
 }
