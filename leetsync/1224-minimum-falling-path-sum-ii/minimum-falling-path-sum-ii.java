2828cf1c2054f2cf5f76ee2b9ac6e20d2dbe7ace
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-04-26 07:41:07 +0530

    Time: 28 ms (76.19%) | Memory: 49.6 MB (75.85%) - LeetSync

--- leetsync/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ leetsync/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,24 +1,22 @@
 class Solution {
-    public int minFallingPathSum(int[][] arr) {
-        int n = arr.length;
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
         for (int row = n - 2; row >= 0; row--) {
             for (int col = 0; col < n; col++) {
                 int min = Integer.MAX_VALUE;
-                // Values to the left.
                 for (int k = 0; k < col; k++) {
-                    min = Math.min(arr[row + 1][k], min);
+                    min = Math.min(min, grid[row + 1][k]);
                 }
-                // Values to the right.
                 for (int k = col + 1; k < n; k++) {
-                    min = Math.min(arr[row + 1][k], min);
+                    min = Math.min(min, grid[row + 1][k]);
                 }
-                arr[row][col] += min;
+                grid[row][col] += min;
             }
         }
-        int result = Integer.MAX_VALUE;
-        for (int i = 0; i < n; ++i) {
-            result = Math.min(result, arr[0][i]);
+        int answer = Integer.MAX_VALUE;
+        for (int col = 0; col < n; col++) {
+            answer = Math.min(answer, grid[0][col]);
         }
-        return result;
+        return answer;
     }
 }
