c296ad76ff5ff6cdc81205b7c25658a50b6dc070
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-01 13:44:03 +0530

    Time: 6 ms (86.28%) | Memory: 45.2 MB (68.80%) - LeetSync

--- leetsync/1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ leetsync/1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,35 +1,15 @@
 class Solution {
-    public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        List<List<Integer>> result = new ArrayList<>();
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < groupSizes.length; i++) {
-            int groupSize = groupSizes[i];
-            if (map.containsKey(groupSize)) {
-                List<Integer> integers = map.get(groupSize);
-                integers.add(i);
-                map.put(groupSize, integers);
-            } else {
-                List<Integer> integers = new ArrayList<>();
-                integers.add(i);
-                map.put(groupSize, integers);
+    public List<List<Integer>> groupThePeople(int[] gz) {
+        List<List<Integer>> res = new ArrayList();
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < gz.length; ++i) {
+            List<Integer> list = groups.computeIfAbsent(gz[i], k -> new ArrayList());
+            list.add(i);
+            if (list.size() == gz[i]) {
+                res.add(list);
+                groups.put(gz[i], new ArrayList());
             }
         }
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
-            Integer key = entry.getKey();
-            List<Integer> integers = entry.getValue();
-            List<Integer> integerList = new ArrayList<>();
-            int listSize = integers.size();
-            int count = key;
-            for (int j = 0; j < listSize; j++, count--) {
-                if (count == 0) {
-                    count = key;
-                    result.add(integerList);
-                    integerList = new ArrayList<>();
-                }
-                integerList.add(integers.get(j));
-            }
-            result.add(integerList);
-        }
-        return result;
+        return res;
     }
 }
