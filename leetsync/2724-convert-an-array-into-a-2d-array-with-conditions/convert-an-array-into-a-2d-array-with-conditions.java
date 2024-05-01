740df899d354a6d4592b89dfb5eb56ac70ed554e
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-01 16:12:02 +0530

    Time: 2 ms (85.90%) | Memory: 45.1 MB (19.50%) - LeetSync

--- leetsync/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ leetsync/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,27 +1,12 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            frequencyMap.put(nums[i], frequencyMap.getOrDefault(nums[i], 0) + 1);
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        int n = nums.length, count[] = new int[n + 1];
+        for (int a : nums) {
+            if (res.size() < ++count[a])
+                res.add(new ArrayList<>());
+            res.get(count[a] - 1).add(a);
         }
-        while (frequencyMap.size() > 0) {
-            List<Integer> integers = new ArrayList<>();
-            for (Iterator<Map.Entry<Integer, Integer>> iterator = frequencyMap.entrySet().iterator(); iterator
-                    .hasNext();) {
-                Map.Entry<Integer, Integer> entry = iterator.next();
-                if (entry.getValue() == 0) {
-                    iterator.remove();
-                } else {
-                    integers.add(entry.getKey());
-                    entry.setValue(entry.getValue() - 1);
-                }
-            }
-            if (integers.size() > 0) {
-                result.add(integers);
-            }
-        }
-        return result;
+        return res;
     }
 }
