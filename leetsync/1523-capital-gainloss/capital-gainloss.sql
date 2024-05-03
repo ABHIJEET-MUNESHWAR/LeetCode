2d2a6d69b044877f7e30789864292d2d6f230d15
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-03 21:18:14 +0530

    Time: 1048 ms (37.64%) | Memory: 0B (100.00%) - LeetSync

--- leetsync/1523-capital-gainloss/capital-gainloss.sql
+++ leetsync/1523-capital-gainloss/capital-gainloss.sql
@@ -1,7 +1,2 @@
 # Write your MySQL query statement below
-#select stock_name, sum(if(operation="Sell", price, -price)) as capital_gain_loss from Stocks group by stock_name
-
-# Write your MySQL query statement below
-select stock_name, sum(case when operation ='Sell' then price else -1*price end) capital_gain_loss
-from Stocks
-group by stock_name
+select stock_name, sum(if(operation="Sell", price, -price)) as capital_gain_loss from Stocks group by stock_name
