c98d92c892d03813dcd0c8470017cda2d2bb4117
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-06 07:53:24 +0530

    Time: 5 ms (82.73%) | Memory: 68.1 MB (30.22%) - LeetSync

--- leetsync/2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ leetsync/2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,42 +1,21 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int size = nums.length;
-        int[] answer = new int[size];
-        int totalNumbersLesserThanPivot = 0;
-        int totalNumbersGreaterThanPivot = 0;
-        int totalNumbersEqualToPivot = 0;
-        for (int i = 0; i < size; i++) {
-            if (nums[i] > pivot) {
-                totalNumbersGreaterThanPivot++;
-            } else if (nums[i] == pivot) {
-                totalNumbersEqualToPivot++;
-            } else {
-                totalNumbersLesserThanPivot++;
-            }
-        }
-        int[] lesserArray = new int[totalNumbersLesserThanPivot];
-        int[] greaterArray = new int[totalNumbersGreaterThanPivot];
-        int[] equalArray = new int[totalNumbersEqualToPivot];
-        int j = 0, k = 0, l = 0;
+        int[] result = new int[size];
+        int left = 0;
+        int right = size - 1;
         for (int i = 0; i < size; i++) {
             if (nums[i] < pivot) {
-                lesserArray[j++] = nums[i];
-            } else if (nums[i] > pivot) {
-                greaterArray[k++] = nums[i];
-            } else {
-                equalArray[l++] = nums[i];
+                result[left++] = nums[i];
+            }
+            if (nums[size - 1 - i] > pivot) {
+                result[right--] = nums[size - 1 - i];
             }
         }
-        int iterator = 0;
-        for (int i = 0; i < lesserArray.length; i++) {
-            answer[iterator++] = lesserArray[i];
+        while (left <= right) {
+            result[left++] = pivot;
+            result[right--] = pivot;
         }
-        for (int i = 0; i < equalArray.length; i++) {
-            answer[iterator++] = equalArray[i];
-        }
-        for (int i = 0; i < greaterArray.length; i++) {
-            answer[iterator++] = greaterArray[i];
-        }
-        return answer;
+        return result;
     }
 }
