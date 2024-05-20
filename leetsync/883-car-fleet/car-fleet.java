a0b89f06eb9ef675c3e877dcba4e24b720847f04
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-20 15:21:14 +0530

    Time: 103 ms (15.40%) | Memory: 61.5 MB (9.84%) - LeetSync

--- leetsync/883-car-fleet/car-fleet.java
+++ leetsync/883-car-fleet/car-fleet.java
@@ -1,14 +1,13 @@
 class Solution {
     public int carFleet(int target, int[] pos, int[] speed) {
-        int N = pos.length, res = 0;
-        double[][] cars = new double[N][2];
-        for (int i = 0; i < N; ++i)
-            cars[i] = new double[] { pos[i], (double) (target - pos[i]) / speed[i] };
-        Arrays.sort(cars, (a, b) -> Double.compare(a[0], b[0]));
+        Map<Integer, Double> m = new TreeMap<>(Collections.reverseOrder());
+        for (int i = 0; i < pos.length; ++i)
+            m.put(pos[i], (double) (target - pos[i]) / speed[i]);
+        int res = 0;
         double cur = 0;
-        for (int i = N - 1; i >= 0; --i) {
-            if (cars[i][1] > cur) {
-                cur = cars[i][1];
+        for (double time : m.values()) {
+            if (time > cur) {
+                cur = time;
                 res++;
             }
         }
