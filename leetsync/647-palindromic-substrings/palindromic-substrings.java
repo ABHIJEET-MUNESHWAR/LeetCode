ce7590ea01a9b1717f8e33a022f1819f9fd992c7
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-21 16:49:19 +0530

    Time: 5 ms (96.64%) | Memory: 41.1 MB (88.36%) - LeetSync

--- leetsync/647-palindromic-substrings/palindromic-substrings.java
+++ leetsync/647-palindromic-substrings/palindromic-substrings.java
@@ -1,42 +1,22 @@
 class Solution {
 
-    int[][] dp = new int[1001][1001];
+    int count = 0;
 
     public int countSubstrings(String s) {
-        for (int i = 0; i < 1001; i++) {
-            for (int j = 0; j < 1001; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        int count = 0;
         int size = s.length();
         for (int i = 0; i < size; i++) {
-            for (int j = i; j < size; j++) {
-                if (isPalindrome(s, i, j)) {
-                    count++;
-                }
-            }
+            isPalindromeFound(s, i, i);
+            isPalindromeFound(s, i, i + 1);
         }
         return count;
     }
 
-    private boolean isPalindrome(String s, int left, int right) {
-        if (left >= right) {
-            return true;
+    private void isPalindromeFound(String s, int i, int j) {
+        int size = s.length();
+        while ((i >= 0) && (j < size) && (s.charAt(i) == s.charAt(j))) {
+            count++;
+            i--;
+            j++;
         }
-        if (dp[left][right] != -1) {
-            return dp[left][right] == 1;
-        }
-        if (s.charAt(left) == s.charAt(right)) {
-            boolean val = isPalindrome(s, left + 1, right - 1);
-            if (val) {
-                dp[left][right] = 1;
-            } else {
-                dp[left][right] = 0;
-            }
-            return val;
-        }
-        dp[left][right] = 0;
-        return false;
     }
 }
