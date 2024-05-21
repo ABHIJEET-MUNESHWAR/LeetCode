3139f77713517f1e1132ff292f8eccb7e78e74a4
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-21 22:01:56 +0530

    Time: 2 ms (99.62%) | Memory: 44.4 MB (27.73%) - LeetSync

--- leetsync/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ leetsync/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,20 +1,19 @@
 class Solution {
 
     public int lengthOfLIS(int[] nums) {
-        int[] t = new int[2501];
-        for (int i = 0; i < 2501; i++) {
-            t[i] = 1;
-        }
         int size = nums.length;
-        int maxLIS = 1;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    t[i] = Math.max(t[i], t[j] + 1);
-                    maxLIS = Math.max(maxLIS, t[i]);
+        int[] dp = new int[size];
+        int length = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, length, num);
+            if (i < 0) {
+                i = -(i + 1);
+                dp[i] = num;
+                if (i == length) {
+                    length++;
                 }
             }
         }
-        return maxLIS;
+        return length;
     }
 }
