81c119870d742241f940ed5eddce32183784a384
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-26 21:31:19 +0530

    Time: 8 ms (43.78%) | Memory: 44.5 MB (26.56%) - LeetSync

--- leetsync/139-word-break/word-break.java
+++ leetsync/139-word-break/word-break.java
@@ -1,29 +1,24 @@
 class Solution {
 
     HashSet<String> set = new HashSet<String>();
-    Boolean[] dp = new Boolean[301];
 
     public boolean wordBreak(String s, List<String> wordDict) {
         set.addAll(wordDict);
-        return solve(s, 0);
+        return solveBottomUp(s);
     }
 
-    private boolean solve(String s, int index) {
-        if (index >= s.length()) {
-            return true;
-        }
-        if (dp[index] != null) {
-            return dp[index];
-        }
-        for (int endIndex = index + 1; endIndex <= s.length(); endIndex++) {
-            String sub = s.substring(index, endIndex);
-            if (set.contains(sub) && solve(s, endIndex)) {
-                dp[index] = true;
-                return true;
+    private boolean solveBottomUp(String s) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        int size = s.length();
+        for (int i = 1; i <= size; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-
-        dp[index] = false;
-        return false;
+        return dp[s.length()];
     }
 }
