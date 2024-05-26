fd7c8b0e4b405ee3e16c577eda253636a52beacf
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-27 00:15:53 +0530

    Time: 767 ms (5.06%) | Memory: 65.2 MB (57.40%) - LeetSync

--- leetsync/552-student-attendance-record-ii/student-attendance-record-ii.java
+++ leetsync/552-student-attendance-record-ii/student-attendance-record-ii.java
@@ -1,19 +1,30 @@
 class Solution {
     public int checkRecord(int n) {
-        final int MOD = 1000000007;
-        int[][][] f = new int[n + 1][2][3];
+        for (int i = 0; i < 100001; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return solve(n, 0, 0);
+    }
 
-        f[0] = new int[][] { { 1, 1, 1 }, { 1, 1, 1 } };
-        for (int i = 1; i <= n; i++)
-            for (int j = 0; j < 2; j++)
-                for (int k = 0; k < 3; k++) {
-                    int val = f[i - 1][j][2]; // ...P
-                    if (j > 0)
-                        val = (val + f[i - 1][j - 1][2]) % MOD; // ...A
-                    if (k > 0)
-                        val = (val + f[i - 1][j][k - 1]) % MOD; // ...L
-                    f[i][j][k] = val;
-                }
-        return f[n][1][2];
+    int mod = 1000000007;
+    int[][][] dp = new int[100001][2][3];
+
+    private int solve(int n, int absent, int consecutiveLate) {
+        // Pruning i.e. ignoring meaningless cases
+        if (absent > 1 || consecutiveLate > 2) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if (dp[n][absent][consecutiveLate] != -1) {
+            return dp[n][absent][consecutiveLate];
+        }
+        int absentCount = solve(n - 1, absent + 1, 0) % mod;
+        int lateCount = solve(n - 1, absent, consecutiveLate + 1) % mod;
+        int presentCount = solve(n - 1, absent, 0) % mod;
+        return dp[n][absent][consecutiveLate] = ((absentCount + lateCount) % mod + presentCount) % mod;
     }
 }
