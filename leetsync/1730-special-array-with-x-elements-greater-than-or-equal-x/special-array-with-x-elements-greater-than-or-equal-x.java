53f503e4e3d86af6ace033ac463dc0d7f1bd53b6
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-27 15:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (12.47%) - LeetSync

--- leetsync/1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ leetsync/1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,17 +1,23 @@
 class Solution {
     public int specialArray(int[] nums) {
         int size = nums.length;
-        for (int i = 0; i <= size; i++) {
-            int totalGreaterElements = 0;
-            for (int j = 0; j < size; j++) {
-                if (nums[j] >= i) {
-                    totalGreaterElements++;
-                }
-            }
-            if (totalGreaterElements == i) {
-                return i;
+        for (int x = 0; x <= size; x++) {
+            int totalGreaterElements = getTotalGreaterElements(nums, x);
+            if (totalGreaterElements == x) {
+                return x;
             }
         }
         return -1;
+    }
+
+    private static int getTotalGreaterElements(int[] nums, int x) {
+        int size = nums.length;
+        int totalGreaterElements = 0;
+        for (int j = 0; j < size; j++) {
+            if (nums[j] >= x) {
+                totalGreaterElements++;
+            }
+        }
+        return totalGreaterElements;
     }
 }
