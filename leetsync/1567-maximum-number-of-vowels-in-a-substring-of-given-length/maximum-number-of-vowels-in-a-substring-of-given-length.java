19b87402faab6aa33cb4f1780bb957fa9d27f65d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-05-29 23:04:02 +0530

    Time: 12 ms (79.99%) | Memory: 44.5 MB (63.61%) - LeetSync

--- leetsync/1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ leetsync/1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxVowels(String s, int k) {
-
         int size = s.length();
         int left = 0, right = 0;
         int maxVowelsLength = 0;
         int currentVowels = 0;
-        Set<Character> vowelSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
         while (right < size) {
-            if (vowelSet.contains(s.charAt(right))) {
+            if (isVowel(s.charAt(right))) {
                 currentVowels++;
             }
             if ((right - left + 1) == k) {
                 maxVowelsLength = Math.max(maxVowelsLength, currentVowels);
-                if (vowelSet.contains(s.charAt(left))) {
+                if (isVowel(s.charAt(left))) {
                     currentVowels--;
                 }
                 left++;
@@ -21,4 +19,9 @@ class Solution {
         }
         return maxVowelsLength;
     }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
 }
