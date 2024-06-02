dcd1079a008f4a75fb1ed118733749558ad462fa
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-02 10:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 49.2 MB (23.64%) - LeetSync

---
+++ leetsync/344-reverse-string/reverse-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int len = s.length;
+        for (int i = 0, j = len - 1; i < j; i++, j--) {
+          char temp = s[i];
+          s[i] = s[j];
+          s[j] = temp;
+        }          
+    }
+}
