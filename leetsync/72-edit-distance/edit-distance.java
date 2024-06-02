57a54453850a3d11515f2f58e0956016f161c47d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-02 21:38:48 +0530

    Time: 5 ms (66.38%) | Memory: 44.7 MB (80.32%) - LeetSync

--- leetsync/72-edit-distance/edit-distance.java
+++ leetsync/72-edit-distance/edit-distance.java
@@ -16,11 +16,11 @@ class Solution {
         if (j == n) {
             return m - i;
         }
-        if (word1.charAt(i) == word2.charAt(j)) {
-            return solve(word1, word2, i + 1, j + 1, m, n, dp);
-        }
         if (dp[i][j] != -1) {
             return dp[i][j];
+        }
+        if (word1.charAt(i) == word2.charAt(j)) {
+            return solve(word1, word2, i + 1, j + 1, m, n, dp);
         }
         int insert = 1 + solve(word1, word2, i, j + 1, m, n, dp);
         int delete = 1 + solve(word1, word2, i + 1, j, m, n, dp);
