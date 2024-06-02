655941a2abff3f96f47b60fcb5a48fa8da089e04
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-02 23:22:26 +0530

    Time: 1 ms (84.44%) | Memory: 42.1 MB (18.86%) - LeetSync

--- leetsync/97-interleaving-string/interleaving-string.java
+++ leetsync/97-interleaving-string/interleaving-string.java
@@ -1,32 +1,48 @@
 class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
-        int size1 = s1.length();
-        int size2 = s2.length();
-        int size3 = s3.length();
-        Boolean[][][] dp = new Boolean[size1 + 1][size2 + 1][size3 + 1];
-        return solve(s1, s2, s3, size1, size2, size3, 0, 0, 0, dp);
-    }
 
-    private boolean solve(String s1, String s2, String s3, int m, int n, int o, int i, int j, int k, Boolean[][][] dp) {
-        if (i >= m && j >= n && k >= o) {
+    int m, n, N;
+    Boolean dp[][];
+
+    public boolean check(String s1, String s2, String s3, int i, int j) {
+        if (i >= m && j >= n && i + j >= N) {
             return true;
         }
-        if (k >= o) {
+
+        if (i + j >= N) {
             return false;
         }
-        if (dp[i][j][k] != null) {
-            return dp[i][j][k];
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
         }
+
         boolean result = false;
-        if (i < m && s1.charAt(i) == s3.charAt(k)) {
-            result = solve(s1, s2, s3, m, n, o, i + 1, j, k + 1, dp);
+
+        if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
+            result = check(s1, s2, s3, i + 1, j);
         }
-        if (result) {
-            return dp[i][j][k] = true;
+
+        if (result == true) {
+            return dp[i][j] = result;
         }
-        if (j < n && s2.charAt(j) == s3.charAt(k)) {
-            result = solve(s1, s2, s3, m, n, o, i, j + 1, k + 1, dp);
+
+        if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
+            result = check(s1, s2, s3, i, j + 1);
         }
-        return dp[i][j][k] = result;
+        return dp[i][j] = result;
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        m = s1.length();
+        n = s2.length();
+        N = s3.length();
+
+        if (m + n != N) {
+            return false;
+        }
+
+        dp = new Boolean[m + 1][n + 1];
+        return check(s1, s2, s3, 0, 0);
+
     }
 }
