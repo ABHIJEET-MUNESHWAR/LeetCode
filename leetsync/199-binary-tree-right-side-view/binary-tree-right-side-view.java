c5f725a28755c7cdadf6097942fe898da06fb7c9
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-03 21:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (99.75%) - LeetSync

--- leetsync/199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ leetsync/199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,29 +14,25 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
 
-        List<Integer> list = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
         if (root == null) {
             return list;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (i == 0) {
-                    list.add(node.val);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-            }
-        }
+        rightSideView(root, 1);
         return list;
+    }
+
+    private void rightSideView(TreeNode root, int currentLevel) {
+        if (root == null) {
+            return;
+        }
+        if (list.size() < currentLevel) {
+            list.add(root.val);
+        }
+        rightSideView(root.right, currentLevel + 1);
+        rightSideView(root.left, currentLevel + 1);
     }
 }
