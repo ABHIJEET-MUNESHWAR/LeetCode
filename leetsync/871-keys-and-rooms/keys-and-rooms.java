98af7c464efa8ae4fdef8a41a44385f1999ba5f6
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-13 13:33:45 +0530

    Time: 2 ms (49.29%) | Memory: 44.1 MB (47.69%) - LeetSync

--- leetsync/871-keys-and-rooms/keys-and-rooms.java
+++ leetsync/871-keys-and-rooms/keys-and-rooms.java
@@ -1,23 +1,33 @@
 class Solution {
 
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int totalRooms = rooms.size();
-        boolean[] visited = new boolean[totalRooms];
-        dfs(rooms, 0, visited);
-        for (int i = 1; i < totalRooms; i++) {
-            if (!visited[i]) {
-                return false;
-            }
-        }
-        return true;
+    int totalRooms = rooms.size();
+    boolean[] visited = new boolean[totalRooms];
+    //dfs(rooms, 0, visited);
+    bfs(rooms, 0, visited);
+    for (int i = 1; i < totalRooms; i++) {
+      if (!visited[i]) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    private void dfs(List<List<Integer>> rooms, int source, boolean[] visited) {
-        visited[source] = true;
-        for (int next : rooms.get(source)) {
-            if (!visited[next]) {
-                dfs(rooms, next, visited);
-            }
+  private void bfs(List<List<Integer>> rooms, int source, boolean[] visited) {
+    visited[source] = true;
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(source);
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        int cur = queue.poll();
+        for (int next : rooms.get(cur)) {
+          if (!visited[next]) {
+            queue.add(next);
+            visited[next] = true;
+          }
         }
+      }
     }
+  }
 }
