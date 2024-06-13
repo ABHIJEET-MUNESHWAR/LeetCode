bf0683f7f8409a8f071f8ee6c45855c6a10da45b
Abhijeet Muneshwar <openingknots@gmail.com>
2024-06-13 20:45:37 +0530

    🟠 [Medium] [Graph] 1443. Minimum Time to Collect All Apples in a Tree

--- src/com/leetcode/medium/graph/MinimumTimeToCollectAllApplesInTree.java
+++ src/com/leetcode/medium/graph/MinimumTimeToCollectAllApplesInTree.java
@@ -3,7 +3,6 @@ package com.leetcode.medium.graph;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
