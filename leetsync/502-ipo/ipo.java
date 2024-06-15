0dab411a35abe001e2db087cd4d1902c1aa2f266
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-15 09:40:34 +0530

    Time: 86 ms (49.34%) | Memory: 61.1 MB (41.59%) - LeetSync

--- leetsync/502-ipo/ipo.java
+++ leetsync/502-ipo/ipo.java
@@ -1,23 +1,23 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = profits.length;
-        int[][] projects = new int[n][2];
-        for (int i = 0; i < n; i++) {
+        int size = profits.length;
+        int[][] projects = new int[size][2];
+        for (int i = 0; i < size; i++) {
             projects[i][0] = capital[i];
             projects[i][1] = profits[i];
         }
-        Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0])); // Sort based on capital value
         int i = 0;
-        PriorityQueue<Integer> maximizeCapital = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> maximiseCapital = new PriorityQueue<>(Collections.reverseOrder());
         while (k-- > 0) {
-            while (i < n && projects[i][0] <= w) {
-                maximizeCapital.offer(projects[i][1]);
+            while (i < size && projects[i][0] <= w) {
+                maximiseCapital.add(projects[i][1]);
                 i++;
             }
-            if (maximizeCapital.isEmpty()) {
+            if (maximiseCapital.isEmpty()) {
                 break;
             }
-            w += maximizeCapital.poll();
+            w += maximiseCapital.poll();
         }
         return w;
     }
