6fa88077956a82330a97e3db05f4230a968519c0
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-16 23:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (7.89%) - LeetSync

--- leetsync/70-climbing-stairs/climbing-stairs.java
+++ leetsync/70-climbing-stairs/climbing-stairs.java
@@ -1,21 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
-    }
-
-    private int solve(int n, int[] dp) {
-        if (n < 0) {
-            return 0;
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if (n == 0) {
-            return 1;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
         return dp[n];
     }
 }
