8c1c4aec8f0887c281a15421fbf5b51d65166ce7
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-16 22:32:56 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (81.36%) - LeetSync

--- leetsync/1013-fibonacci-number/fibonacci-number.java
+++ leetsync/1013-fibonacci-number/fibonacci-number.java
@@ -1,21 +1,11 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) {
-            return n;
-        }
         int[] dp = new int[31];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
-    }
-
-    private int solve(int n, int[] dp) {
-        if (n <= 1) {
-            return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= 30; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
         return dp[n];
     }
 }
