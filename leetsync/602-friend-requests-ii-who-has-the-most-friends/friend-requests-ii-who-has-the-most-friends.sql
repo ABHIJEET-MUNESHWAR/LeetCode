dc55b7e7ed5b4be3708f9b9e8c884502ecfce69d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-17 23:17:23 +0530

    Time: 295 ms (91.44%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetsync/602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,7 @@
+select id, count(*) num from 
+(
+      (select requester_id id from RequestAccepted) 
+      union all 
+      (select accepter_id id from RequestAccepted)
+) tb 
+group by id order by num desc limit 1
