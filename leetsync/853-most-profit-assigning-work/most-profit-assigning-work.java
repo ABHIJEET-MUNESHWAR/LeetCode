3de21bc31c54ddaa0a15c5745c261fd5a3da6db0
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-18 14:36:58 +0530

    Time: 17 ms (86.61%) | Memory: 44.9 MB (98.53%) - LeetSync

--- leetsync/853-most-profit-assigning-work/most-profit-assigning-work.java
+++ leetsync/853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -3,30 +3,22 @@ class Solution {
         int workerSize = worker.length;
         int difficultySize = difficulty.length;
         int totalProfit = 0;
-        List<int[]> jobsList = new ArrayList<>();
+        // Create an array of pairs to hold difficulty and profit
+        int[][] jobs = new int[difficultySize][2];
         for (int i = 0; i < difficultySize; i++) {
-            jobsList.add(new int[] { difficulty[i], profit[i] });
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
         }
         // Sort the vector based on difficulty
-        Collections.sort(jobsList, (a, b) -> Integer.compare(a[0], b[0]));
-        // Pre-processing to find the maximum profit till index i at constant time
-        for (int i = 1; i < difficultySize; i++) {
-            jobsList.get(i)[1] = Math.max(jobsList.get(i)[1], jobsList.get(i - 1)[1]);
-        }
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
+        // Sort the worker array
+        Arrays.sort(worker);
+        int j = 0; // Pointer to vec array
+        int maxProfit = 0;
         for (int i = 0; i < workerSize; i++) {
-            int workerDifficulty = worker[i];
-            // Apply binary search on vec
-            int low = 0;
-            int high = difficultySize - 1;
-            int maxProfit = 0;
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-                if (jobsList.get(mid)[0] <= workerDifficulty) {
-                    maxProfit = Math.max(maxProfit, jobsList.get(mid)[1]);
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+            while (j < difficultySize && worker[i] >= jobs[j][0]) {
+                maxProfit = Math.max(maxProfit, jobs[j][1]);
+                j++;
             }
             totalProfit += maxProfit;
         }
