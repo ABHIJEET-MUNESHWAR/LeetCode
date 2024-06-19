1208058c0ed015b0451a4ca81afd07bae5203396
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-19 22:54:53 +0530

    Time: 39 ms (47.36%) | Memory: 44.4 MB (22.67%) - LeetSync

--- leetsync/279-perfect-squares/perfect-squares.java
+++ leetsync/279-perfect-squares/perfect-squares.java
@@ -1,24 +1,14 @@
 class Solution {
 
-    int[] dp = new int[10001];
-
     public int numSquares(int n) {
-        Arrays.fill(dp, -1);
-        return solveRecursionMemoization(n);
-    }
-
-    private int solveRecursionMemoization(int n) {
-        if (n == 0) {
-            return 0;
+        int[] dp = new int[10001];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
         }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        int minCount = Integer.MAX_VALUE;
-        for (int i = 1; i * i <= n; i++) {
-            int result = 1 + solveRecursionMemoization(n - i * i);
-            minCount = Math.min(minCount, result);
-        }
-        return dp[n] = minCount;
+        return dp[n];
     }
 }
