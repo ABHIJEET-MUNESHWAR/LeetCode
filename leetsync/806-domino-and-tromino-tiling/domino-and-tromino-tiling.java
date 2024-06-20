8edf82585fdbf1c90d80070c3f52173d56f20e47
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-20 14:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (73.23%) - LeetSync

--- leetsync/806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
+++ leetsync/806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -2,25 +2,20 @@ class Solution {
 
     int mod = 1000000007;
 
-    int[] dp;
-
     public int numTilings(int n) {
-        dp = new int[1001];
-        Arrays.fill(dp, -1);
-        return numTilingsRecursionAndMemoization(n);
-    }
-
-    private int numTilingsRecursionAndMemoization(int n) {
         if (n == 1 || n == 2) {
             return n;
         }
         if (n == 3) {
             return 5;
         }
-        if (dp[n] != -1) {
-            return dp[n];
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] % mod + dp[i - 3] % mod) % mod;
         }
-        return dp[n] = (2 * numTilingsRecursionAndMemoization(n - 1) % mod
-                + numTilingsRecursionAndMemoization(n - 3) % mod) % mod;
+        return dp[n];
     }
 }
