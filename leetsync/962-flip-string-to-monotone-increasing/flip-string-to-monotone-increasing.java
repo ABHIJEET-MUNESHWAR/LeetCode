1fdc35c0b877949fb984f6ec14cb3577b34ce62b
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-20 20:57:30 +0530

    Time: 7 ms (96.30%) | Memory: 45.2 MB (58.64%) - LeetSync

--- leetsync/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
+++ leetsync/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -1,44 +1,17 @@
 class Solution {
-    int[][] dp;
 
     public int minFlipsMonoIncr(String s) {
+        char[] chars = s.toCharArray();
         int size = s.length();
-        dp = new int[size + 1][2];
-        for (int i = 0; i < size; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return solveRecursionMemoization(s.toCharArray(), 0, 0);
-    }
-
-    private int solveRecursionMemoization(char[] chars, int previous, int currentIndex) {
-        int flipCount = Integer.MAX_VALUE;
-        int nonFlipCount = Integer.MAX_VALUE;
-        if (currentIndex >= chars.length) {
-            return 0;
-        }
-        if (dp[currentIndex][previous] != -1) {
-            return dp[currentIndex][previous];
-        }
-        if (chars[currentIndex] == '0') {
-            if (previous == 1) {
-                // 1, 0
-                flipCount = 1 + solveRecursionMemoization(chars, 1, currentIndex + 1);
+        int countOfOnce = 0;
+        int flips = 0;
+        for (char ch : chars) {
+            if (ch == '1') {
+                countOfOnce++;
             } else {
-                // 0, 0
-                flipCount = 1 + solveRecursionMemoization(chars, 1, currentIndex + 1);
-                nonFlipCount = solveRecursionMemoization(chars, 0, currentIndex + 1);
-            }
-        } else {
-            // current char = 1
-            if (previous == 1) {
-                // 1, 1
-                nonFlipCount = solveRecursionMemoization(chars, 1, currentIndex + 1);
-            } else {
-                // 0, 1
-                flipCount = 1 + solveRecursionMemoization(chars, 0, currentIndex + 1);
-                nonFlipCount = solveRecursionMemoization(chars, 1, currentIndex + 1);
+                flips = Math.min(flips + 1, countOfOnce);
             }
         }
-        return dp[currentIndex][previous] = Math.min(flipCount, nonFlipCount);
+        return flips;
     }
 }
