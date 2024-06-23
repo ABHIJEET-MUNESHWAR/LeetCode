4e2eaf0a41f64bf864a400b8f557cf6414f266f5
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-23 16:51:05 +0530

    Time: 141 ms (70.74%) | Memory: 184.9 MB (5.77%) - LeetSync

--- leetsync/1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ leetsync/1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,40 +1,25 @@
 class Solution {
 
-    int[][] dp;
-
     public int longestArithSeqLength(int[] nums) {
-        int size = nums.length;
-
-        if (size <= 2) {
-            return size;
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
         }
-        dp = new int[1001][1005];
-        for (int i = 0; i < 1001; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        int result = 0;
-        for (int i = 1; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                int diff = nums[j] - nums[i];
-                result = Math.max(result, 2 + solveRecursionMemoization(nums, i, diff));
-            }
-        }
-        return result;
-    }
-
-    private int solveRecursionMemoization(int[] nums, int index, int diff) {
-        int result = 0;
-        if (index < 0) {
+        if (nums == null) {
             return 0;
         }
-        if (dp[index][diff + 501] != -1) {
-            return dp[index][diff + 501];
-        }
-        for (int k = index - 1; k >= 0; k--) {
-            if ((nums[index] - nums[k]) == diff) {
-                result = Math.max(result, 1 + solveRecursionMemoization(nums, k, diff));
+        int[][] dp = new int[nums.length][20001];
+
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                int diff = nums[i] - nums[j] + 10000;
+                dp[i][diff] = Math.max(dp[i][diff], dp[j][diff] + 1); // DON'T forget to compare
+
+                res = Math.max(res, dp[i][diff]);
             }
         }
-        return dp[index][diff + 501] = result;
+
+        return res + 1;
     }
 }
