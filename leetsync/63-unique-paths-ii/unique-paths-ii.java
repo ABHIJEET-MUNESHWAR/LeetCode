30a4f621231b56809f5e5bb6ed03ec9b285ba417
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-25 08:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (95.89%) - LeetSync

--- leetsync/63-unique-paths-ii/unique-paths-ii.java
+++ leetsync/63-unique-paths-ii/unique-paths-ii.java
@@ -1,30 +1,21 @@
 class Solution {
-    int[][] dp;
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
         int row = obstacleGrid.length;
         int col = obstacleGrid[0].length;
         if (obstacleGrid[row - 1][col - 1] == 1) {
             return 0;
         }
-        dp = new int[row][col];
-        for (int i = 0; i < row; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[row + 1][col + 1];
+        dp[0][1] = 1;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (obstacleGrid[i - 1][j - 1] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
         }
-        return solveRecursionMemoization(obstacleGrid, 0, 0);
-    }
-
-    private int solveRecursionMemoization(int[][] obstacleGrid, int i, int j) {
-        if (i == obstacleGrid.length - 1 && j == obstacleGrid[0].length - 1) {
-            return 1;
-        }
-        if (i < 0 || i >= obstacleGrid.length || j < 0 || j >= obstacleGrid[0].length || obstacleGrid[i][j] == 1) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        return dp[i][j] = solveRecursionMemoization(obstacleGrid, i + 1, j)
-                + solveRecursionMemoization(obstacleGrid, i, j + 1);
+        return dp[row][col];
     }
 }
