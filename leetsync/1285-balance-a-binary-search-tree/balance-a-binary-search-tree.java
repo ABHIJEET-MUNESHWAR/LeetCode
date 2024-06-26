c57df4d52ba5d44144e6567ebd544a3a45c3b008
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-26 07:31:33 +0530

    Time: 2 ms (96.27%) | Memory: 45.5 MB (86.67%) - LeetSync

--- leetsync/1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ leetsync/1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -14,32 +14,29 @@
  * }
  */
 class Solution {
-    List<Integer> sortedArray = new ArrayList<>();
     public TreeNode balanceBST(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        convertBstToSortedArray(root);
-        return balanceBST(0, sortedArray.size() - 1);
+        List<Integer> sortedArray = new ArrayList<>();
+        inorder(root, sortedArray);
+        return constructBalancedBst(sortedArray, 0, sortedArray.size() - 1);
     }
 
-    private void convertBstToSortedArray(TreeNode root) {
+    private TreeNode constructBalancedBst(List<Integer> sortedArray, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(sortedArray.get(mid));
+        root.left = constructBalancedBst(sortedArray, left, mid - 1);
+        root.right = constructBalancedBst(sortedArray, mid + 1, right);
+        return root;
+    }
+
+    private void inorder(TreeNode root, List<Integer> sortedArray) {
         if (root == null) {
             return;
         }
-        convertBstToSortedArray(root.left);
+        inorder(root.left, sortedArray);
         sortedArray.add(root.val);
-        convertBstToSortedArray(root.right);
-    }
-
-    private TreeNode balanceBST(int start, int end) {
-        if (start > end) {
-            return null;
-        }
-        int mid = (start + end) / 2;
-        TreeNode root = new TreeNode(sortedArray.get(mid));
-        root.left = balanceBST(start, mid - 1);
-        root.right = balanceBST(mid + 1, end);
-        return root;
+        inorder(root.right, sortedArray);
     }
 }
