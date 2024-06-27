092c2272e98d7dcbe1487d537e0961936136efbc
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-06-27 20:04:03 +0530

    Time: 11 ms (20.84%) | Memory: 42.3 MB (45.71%) - LeetSync

--- leetsync/576-out-of-boundary-paths/out-of-boundary-paths.java
+++ leetsync/576-out-of-boundary-paths/out-of-boundary-paths.java
@@ -1,36 +1,24 @@
 class Solution {
     int mod = 1000000007;
     int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-    int[][][] dp;
 
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        dp = new int[51][51][51];
-        for (int i = 0; i < 51; i++) {
-            for (int j = 0; j < 51; j++) {
-                for (int k = 0; k < 51; k++) {
-                    dp[i][j][k] = -1;
+        int[][][] dp = new int[m][n][maxMove + 1];
+        for (int k = 1; k <= maxMove; k++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int[] direction : directions) {
+                        int x = i + direction[0];
+                        int y = j + direction[1];
+                        if (x < 0 || y < 0 || x >= m || y >= n) {
+                            dp[i][j][k] = (dp[i][j][k] + 1) % mod;
+                        } else {
+                            dp[i][j][k] = (dp[i][j][k] + dp[x][y][k - 1]) % mod;
+                        }
+                    }
                 }
             }
         }
-        return solveRecursionMemoization(m, n, maxMove, startRow, startColumn);
-    }
-
-    private int solveRecursionMemoization(int m, int n, int maxMove, int startRow, int startColumn) {
-        if (startRow < 0 || startColumn < 0 || startRow >= m || startColumn >= n) {
-            return 1;
-        }
-        if (maxMove <= 0) {
-            return 0;
-        }
-        if (dp[startRow][startColumn][maxMove] != -1) {
-            return dp[startRow][startColumn][maxMove];
-        }
-        int result = 0;
-        for (int[] direction : directions) {
-            result = (result
-                    + solveRecursionMemoization(m, n, maxMove - 1, startRow + direction[0], startColumn + direction[1]))
-                    % mod;
-        }
-        return dp[startRow][startColumn][maxMove] = result;
+        return dp[startRow][startColumn][maxMove];
     }
 }
