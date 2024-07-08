e21ac4be165cf7dfa733fd113f117ee6f35ba10e
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-07-08 14:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.17%) - LeetSync

--- leetsync/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ leetsync/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,12 +1,14 @@
 class Solution {
-    public int findTheWinner(int n, int k) {
-        return solve(n, k) + 1;
-    }
-
     private int solve(int n, int k) {
         if (n == 1) {
             return 0;
         }
-        return (k + solve(n - 1, k)) % n;
+        int index = solve(n - 1, k);
+        index = (index + k) % n;
+        return index;
+    }
+
+    public int findTheWinner(int n, int k) {
+        return solve(n, k) + 1;
     }
 }
