5c2650c3a43b6c6cb7d712c362922dfbbcccadd3
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-07-14 15:27:13 +0530

    Time: 7 ms (97.45%) | Memory: 45.3 MB (58.55%) - LeetSync

--- leetsync/1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ leetsync/1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int minDeletions(String s) {        
+    public int minDeletions(String s) {
+        int n = s.length();
         char[] chars = s.toCharArray();
-        int n = chars.length;
-        int[] frequencyMap = new int[26];
         int result = 0;
+        int[] frequencyMap = new int[26];
         for (int i = 0; i < n; i++) {
             frequencyMap[chars[i] - 'a']++;
         }
-        Set<Integer> frequencySet = new HashSet<>();
-        for (int i = 0; i < 26; i++) {
-            while (frequencyMap[i] > 0 && frequencySet.contains(frequencyMap[i])) {
-                frequencyMap[i]--;
-                result++;
+        Arrays.sort(frequencyMap);
+        for (int i = 24; i >= 0 && frequencyMap[i] > 0; i--) {
+            if (frequencyMap[i] >= frequencyMap[i + 1]) {
+                int previous = frequencyMap[i];
+                frequencyMap[i] = Math.max(0, frequencyMap[i + 1] - 1);
+                result += (previous - frequencyMap[i]);
             }
-            frequencySet.add(frequencyMap[i]);
         }
         return result;
     }
