067568709ed6a4c3f2c9d98c326fb90082062a3e
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-07-17 08:25:09 +0530

    Time: 43 ms (78.98%) | Memory: 58.1 MB (45.06%) - LeetSync

--- leetsync/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ leetsync/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,25 +1,19 @@
 class Solution {
-    public int findLeastNumOfUniqueInts(int[] arr, int k) {Map<Integer, Integer> mp = new HashMap<>();
-
-        for (int x : arr) {
-            mp.put(x, mp.getOrDefault(x, 0) + 1);
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> numToFrequencyMap = new HashMap<>();
+        for(int num: arr){
+            numToFrequencyMap.put(num, numToFrequencyMap.getOrDefault(num, 0) + 1);
         }
-
-        List<Integer> freq = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            freq.add(entry.getValue());
-        }
-
-        Collections.sort(freq);
-
-        for (int i = 0; i < freq.size(); i++) {
-            k -= freq.get(i);
-
+        List<Integer> frequencyList = new ArrayList<>();
+        frequencyList.addAll(numToFrequencyMap.values());
+        Collections.sort(frequencyList);
+        int size = frequencyList.size();
+        for (int i = 0; i < size; i++) {
+            k -= frequencyList.get(i);
             if (k < 0) {
-                return freq.size() - i;
+                return size - i;
             }
         }
-
         return 0; // All were removed
     }
 }
