f7acd9d794b2aa438f9cf2759cad9358c6da8238
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-07-17 08:29:21 +0530

    Time: 53 ms (46.88%) | Memory: 56.5 MB (82.05%) - LeetSync

--- leetsync/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ leetsync/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,18 +1,17 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
         Map<Integer, Integer> numToFrequencyMap = new HashMap<>();
-        for(int num: arr){
+        for (int num : arr) {
             numToFrequencyMap.put(num, numToFrequencyMap.getOrDefault(num, 0) + 1);
         }
-        List<Integer> frequencyList = new ArrayList<>();
-        frequencyList.addAll(numToFrequencyMap.values());
-        Collections.sort(frequencyList);
-        int size = frequencyList.size();
-        for (int i = 0; i < size; i++) {
-            k -= frequencyList.get(i);
-            if (k < 0) {
-                return size - i;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(numToFrequencyMap.values());
+        int removedElements = 0;
+        while (!minHeap.isEmpty()) {
+            removedElements += minHeap.peek();
+            if (removedElements > k) {
+                return minHeap.size();
             }
+            minHeap.poll();
         }
         return 0; // All were removed
     }
