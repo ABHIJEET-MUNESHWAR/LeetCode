edba4e4437b2dbda93b91e6978f714ebea18b176
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-07-21 20:48:13 +0530

    Time: 10 ms (82.68%) | Memory: 55.4 MB (40.94%) - LeetSync

--- leetsync/2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.java
+++ leetsync/2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
         int[][] matrix = new int[k][k];
-        List<Integer> topologicalRows = topologicalSort(rowConditions, k);
-        List<Integer> topologicalCols = topologicalSort(colConditions, k);
+        List<Integer> topologicalRows = topologicalSortBfs(rowConditions, k);
+        List<Integer> topologicalCols = topologicalSortBfs(colConditions, k);
         if (topologicalCols.isEmpty() || topologicalRows.isEmpty()) {
-            return new int[][]{}; // Found cycle. Answer is not possible.
+            return new int[][] {}; // Found cycle. Answer is not possible.
         }
         Map<Integer, Integer> positionMap = new HashMap<>();
         for (int i = 0; i < k; i++) {
@@ -19,42 +19,42 @@ class Solution {
         return matrix;
     }
 
-    private List<Integer> topologicalSort(int[][] edges, int n) {
+    private List<Integer> topologicalSortBfs(int[][] edges, int n) {
         // Build adjacency list
-        Map<Integer, List<Integer>> adjacencyMap = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        int[] inDegrees = new int[n + 1];
         List<Integer> order = new ArrayList<>();
-        int[] visited = new int[n + 1]; // 0: not visited, 1: DFS is in progress, 2: visited
-        boolean[] hasCycle = { false };
-        for (int[] edge : edges) {
-            adjacencyMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+        for (int i = 0; i <= n; i++) {
+            adjacencyList.add(new ArrayList<>());
         }
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adjacencyList.get(u).add(v);
+            inDegrees[v]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
         for (int i = 1; i <= n; i++) {
-            if (visited[i] == 0) {
-                dfs(i, adjacencyMap, visited, stack, hasCycle);
-                if (hasCycle[0]) {
-                    return new ArrayList<>(); // no ordering possible
+            if (inDegrees[i] == 0) {
+                queue.add(i);
+                count++;
+            }
+        }
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            order.add(u);
+            for (int v : adjacencyList.get(u)) {
+                inDegrees[v]--;
+                if (inDegrees[v] == 0) {
+                    queue.add(v);
+                    count++;
                 }
             }
         }
-        while (!stack.isEmpty()) {
-            order.add(stack.pop());
+        if (count != n) {
+            return new ArrayList<>(); // Cycle detected
         }
         return order;
-    }
-
-    private void dfs(int node, Map<Integer, List<Integer>> adjacencyMap, int[] visited, Stack<Integer> stack,
-            boolean[] hasCycle) {
-        visited[node] = 1; // Mark node as visiting
-        for (int neighbour : adjacencyMap.getOrDefault(node, new ArrayList<>())) {
-            if (visited[neighbour] == 0) {
-                dfs(neighbour, adjacencyMap, visited, stack, hasCycle);
-            } else if (visited[neighbour] == 1) {
-                hasCycle[0] = true; // Cycle detected
-                return;
-            }
-        }
-        visited[node] = 2; // Mark node as visited
-        stack.push(node); // Add node to the stack
     }
 }
