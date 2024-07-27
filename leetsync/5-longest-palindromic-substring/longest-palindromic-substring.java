e6510b44cb3d30747c3842603db64f6d348fccb8
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-07-27 21:40:39 +0530

    Time: 111 ms (44.55%) | Memory: 46.5 MB (11.41%) - LeetSync

--- leetsync/5-longest-palindromic-substring/longest-palindromic-substring.java
+++ leetsync/5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,39 +1,24 @@
 class Solution {
-
-    int[][] dp = new int[1001][1001];
-
     public String longestPalindrome(String s) {
         int n = s.length();
-        int maxLength = 0;
+        boolean[][] dp = new boolean[n][n];
+        int maxLength = 1;
         int startIndex = 0;
-        for (int i = 0; i < 1001; i++) {
-            Arrays.fill(dp[i], -1);
-        }
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(s, i, j)) {
-                    if (maxLength < (j - i + 1)) {
-                        maxLength = j - i + 1;
+            dp[i][i] = true;
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i + l <= n; i++) {
+                int j = i + l - 1;
+                if (s.charAt(i) == s.charAt(j) && (dp[i + 1][j - 1] || l == 2)) {
+                    dp[i][j] = true;
+                    if (maxLength < l) {
+                        maxLength = l;
                         startIndex = i;
                     }
                 }
             }
         }
         return s.substring(startIndex, startIndex + maxLength);
-    }
-
-    public boolean isPalindrome(String s, int i, int j) {
-        if (i >= j) {
-            return true;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j] == 1;
-        }
-        if (s.charAt(i) == s.charAt(j)) {
-            dp[i][j] = isPalindrome(s, i + 1, j - 1) ? 1 : 0;
-        } else {
-            dp[i][j] = 0;
-        }
-        return dp[i][j] == 1;
     }
 }
