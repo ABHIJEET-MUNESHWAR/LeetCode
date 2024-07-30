743c254b82470b898df0c979cd0fc75554585d78
Abhijeet Muneshwar <openingknots@gmail.com>
2024-07-30 12:02:21 +0530

    🟢 [Easy] [Adyen] Sort words based on word length preserving the sequence of words

---
+++ src/com/leetcode/companies/adyen/SortWords.java
@@ -0,0 +1,40 @@
+package com.leetcode.companies.adyen;
+
+/**
+ * Created using IntelliJ IDEA.
+ * Author:  abhijeet,
+ * Date:    30/07/24,
+ * Time:    11:10 am
+ * // Sort words based on word length preserving the sequence of words
+ */
+public class SortWords {
+    public static void main(String[] args) {
+        SortWords sortWords = new SortWords();
+        String str = "This is repeated string of length some value.";
+        System.out.println(sortWords.sortWords(str));
+    }
+
+    private String sortWords(String str) {
+        int n = str.length();
+        str = str.substring(0, n - 1);
+        String[] words = str.split(" ");
+        n = words.length;
+        for (int i = 1; i < n; i++) {
+            String temp = words[i];
+            // Insert s[j] at its correct position
+            int j = i - 1;
+            while (j >= 0 && temp.length() < words[j].length()) {
+                words[j + 1] = words[j];
+                j--;
+            }
+            words[j + 1] = temp;
+        }
+        String output = String.join(" ", words);
+        output = output.toLowerCase();
+        StringBuilder sb = new StringBuilder(output);
+        sb.setCharAt(0, Character.toUpperCase(output.charAt(0)));
+        output = sb.toString();
+        output = output + ".";
+        return output;
+    }
+}
