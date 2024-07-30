a4b28031ecc82d68079f1f664513c8aa83838acf
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-07-30 18:14:38 +0530

    Time: 41 ms (33.15%) | Memory: 45.8 MB (9.47%) - LeetSync

--- leetsync/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ leetsync/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,19 +1,28 @@
 class Solution {
-    public int minimumDeletions(String s) {   
-        Stack<Character> stack = new Stack<>();
-        int deletedCount = 0;
+    public int minimumDeletions(String s) {
         int n = s.length();
+        int[] leftB = new int[n];
+        int[] rightA = new int[n];
+        int leftBCount = 0;
+        int rightACount = 0;
+        int result = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
+            leftB[i] = leftBCount;
             if (c == 'b') {
-                stack.push(c);
-            } else {
-                if (!stack.isEmpty() && stack.peek() == 'b') {
-                    deletedCount++;
-                    stack.pop();
-                }
+                leftBCount++;
             }
         }
-        return deletedCount;
+        for (int i = n - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            rightA[i] = rightACount;
+            if (c == 'a') {
+                rightACount++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            result = Math.min(result, leftB[i] + rightA[i]);
+        }
+        return result;
     }
 }
