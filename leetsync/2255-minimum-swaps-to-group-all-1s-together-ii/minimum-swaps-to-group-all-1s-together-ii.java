90d9f5fecb55745be0c24352550cb4f108bfaa7a
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-08-03 11:43:07 +0530

    Time: 9 ms (58.88%) | Memory: 59.1 MB (65.74%) - LeetSync

--- leetsync/2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ leetsync/2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,12 +1,24 @@
 class Solution {
     public int minSwaps(int[] nums) {
-        int ones = Arrays.stream(nums).sum(), n = nums.length, res = nums.length;
-    for (int i = 0, j = 0, cnt = 0; i < n; ++i) {
-        while (j - i < ones)
-            cnt += nums[j++ % n];
-        res = Math.min(res, ones - cnt);
-        cnt -= nums[i];
-    }
-    return res;
+        int n = nums.length;
+        int maxOnce = 0;
+        for (int num : nums) {
+            maxOnce += num;
+        }
+        int left = 0, right = 0, currentOnce = 0, currentMaxOnce = 0;
+        while (right < 2 * n) {
+            if (nums[right % n] == 1) {
+                currentOnce++;
+            }
+            int currentWindowSize = right - left + 1;
+            if (currentWindowSize > maxOnce) {
+                currentOnce -= nums[left % n];
+                left++;
+            }
+
+            currentMaxOnce = Math.max(currentMaxOnce, currentOnce);
+            right++;
+        }
+        return maxOnce - currentMaxOnce;
     }
 }
