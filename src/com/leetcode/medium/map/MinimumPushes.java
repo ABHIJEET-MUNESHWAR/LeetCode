24908a9f90cffdd187392a07e33ab417829f235f
Abhijeet Muneshwar <openingknots@gmail.com>
2024-08-09 13:07:11 +0530

    🟠 [Medium] [Hash Table] [Map] [Math] 3014. Minimum Number of Pushes to Type Word

---
+++ src/com/leetcode/medium/map/MinimumPushes.java
@@ -0,0 +1,21 @@
+package com.leetcode.medium.map;
+
+/**
+ * Created using IntelliJ IDEA.
+ * Author:  abhijeet,
+ * Date:    09/08/24,
+ * Time:    10:36 am
+ * 3016. Minimum Number of Pushes to Type Word II
+ * https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/
+ */
+public class MinimumPushes {
+    public static void main(String[] args) {
+        MinimumPushes minimumPushes = new MinimumPushes();
+        String word = "abcde";
+        System.out.println(minimumPushes.minimumPushes(word));
+    }
+
+    private int minimumPushes(String word) {
+        return 0;
+    }
+}
