6ac5ffaecebe4995e0c7d6e2a6b7dd89f66c18dd
Abhijeet Muneshwar <openingknots@gmail.com>
2024-08-12 21:09:52 +0530

    🟠 [Medium] [Matrix] [Simulation] 54. Spiral Matrix

---
+++ src/com/leetcode/medium/simulation/SpiralMatrixThree.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium.simulation;
+
+import com.leetcode.utils.ArrayUtils;
+
+/**
+ * Created using IntelliJ IDEA.
+ * Author:  abhijeet,
+ * Date:    12/08/24,
+ * Time:    8:13 pm
+ * 885. Spiral Matrix III
+ * https://leetcode.com/problems/spiral-matrix-iii/description/
+ */
+public class SpiralMatrixThree {
+    public static void main(String[] args) {
+        SpiralMatrixThree spiralMatrixThree = new SpiralMatrixThree();
+        int rows = 1, cols = 4, rStart = 0, cStart = 0;
+        ArrayUtils.printMatrix(spiralMatrixThree.spiralMatrixThree(rows, cols, rStart, cStart));
+    }
+
+    private int[][] spiralMatrixThree(int rows, int cols, int rStart, int cStart) {
+        return null;
+    }
+}
