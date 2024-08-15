51f82163833523eb8fb785e2e5ab23134fae6a53
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-08-15 21:06:57 +0530

    Time: 386 ms (9.64%) | Memory: 44.5 MB (29.09%) - LeetSync

--- leetsync/719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ leetsync/719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,27 +1,20 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-        Arrays.sort(nums);
-
         int n = nums.length;
-        int l = 0;
-        int r = nums[n - 1] - nums[0];
-
-        for (int cnt = 0; l < r; cnt = 0) {
-            int m = l + (r - l) / 2;
-
-            for (int i = 0, j = 0; i < n; i++) {
-                while (j < n && nums[j] <= nums[i] + m)
-                    j++;
-                cnt += j - i - 1;
-            }
-
-            if (cnt < k) {
-                l = m + 1;
-            } else {
-                r = m;
+        int max = Arrays.stream(nums).max().getAsInt();
+        int[] temp = new int[max + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int d = Math.abs(nums[i] - nums[j]);
+                temp[d]++;
             }
         }
-
-        return l;
+        for (int distance = 0; distance < max; distance++) {
+            k -= temp[distance];
+            if (k <= 0) {
+                return distance; // Returning kth smallest distance
+            }
+        }
+        return -1;
     }
 }
