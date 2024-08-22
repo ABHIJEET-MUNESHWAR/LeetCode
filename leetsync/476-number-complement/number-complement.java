7d5a500fff323ba20716d8f0ff1d1bf89b063185
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-08-22 21:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (20.00%) - LeetSync

--- leetsync/476-number-complement/number-complement.java
+++ leetsync/476-number-complement/number-complement.java
@@ -1,12 +1,9 @@
 class Solution {
     public int findComplement(int num) {
-        
-        int mask = num;
-        mask |= mask >> 1;
-        mask |= mask >> 2;
-        mask |= mask >> 4;
-        mask |= mask >> 8;
-        mask |= mask >> 16;
-        return num ^ mask;
+        int noOfBits = (int) (Math.log(num) / Math.log(2)) + 1;
+        for (int i = 0; i < noOfBits; i++) {
+            num = num ^ (1 << i);
+        }
+        return num;
     }
 }
