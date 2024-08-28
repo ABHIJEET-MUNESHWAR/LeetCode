0dde73a0e7cf821ca42c2f4ed67739c3470b8e70
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-08-29 00:31:17 +0530

    Time: 21 ms (61.19%) | Memory: 85.9 MB (28.82%) - LeetSync

--- leetsync/2035-count-sub-islands/count-sub-islands.java
+++ leetsync/2035-count-sub-islands/count-sub-islands.java
@@ -1,22 +1,30 @@
 class Solution {
-        public int countSubIslands(int[][] B, int[][] A) {
-        int m = A.length, n = A[0].length, res = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (A[i][j] == 1)
-                    res += dfs(B, A, i, j);
-        return res;
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    result += dfs(grid1, grid2, i, j);
+                }
+            }
+        }
+        return result;
     }
 
-
-    private int dfs(int[][] B, int[][] A, int i, int j) {
-        int m = A.length, n = A[0].length, res = 1;
-        if (i < 0 || i == m || j < 0 || j == n || A[i][j] == 0) return 1;
-        A[i][j] = 0;
-        res &= dfs(B, A, i - 1, j);
-        res &= dfs(B, A, i + 1, j);
-        res &= dfs(B, A, i, j - 1);
-        res &= dfs(B, A, i, j + 1);
-        return res & B[i][j];
+    private int dfs(int[][] grid1, int[][] grid2, int i, int j) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+        int result = 1;
+        if (i < 0 || i == m || j < 0 || j == n || grid2[i][j] == 0) {
+            return 1;
+        }
+        grid2[i][j] = 0;
+        result &= dfs(grid1, grid2, i - 1, j);
+        result &= dfs(grid1, grid2, i + 1, j);
+        result &= dfs(grid1, grid2, i, j - 1);
+        result &= dfs(grid1, grid2, i, j + 1);
+        return result & grid1[i][j];
     }
 }
