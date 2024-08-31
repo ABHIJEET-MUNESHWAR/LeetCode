8d8d381680f73a9fd85bc4b616af431b394c31b6
Abhijeet Muneshwar <openingknots@gmail.com>
2024-08-31 15:41:32 +0530

    🟠 [Medium] [Graph] [DSU] 947. Most Stones Removed with Same Row or Column

--- src/com/leetcode/medium/graph/RemoveStones.java
+++ src/com/leetcode/medium/graph/RemoveStones.java
@@ -9,55 +9,56 @@ package com.leetcode.medium.graph;
  * https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/
  */
 public class RemoveStones {
-  public static void main(String[] args) {
-    RemoveStones removeStones = new RemoveStones();
-    int[][] stones = {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
-    System.out.println(removeStones.removeStones(stones));
-  }
+    public static void main(String[] args) {
+        RemoveStones removeStones = new RemoveStones();
+        int[][] stones = {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
+        System.out.println(removeStones.removeStones(stones));
+    }
 
-  public int find(int i, int[] parent) {
-    if (parent[i] == i) {
-      return i;
-    }
-    return parent[i] = find(parent[i], parent);
-  }
-
-  public void union(int i, int j, int[] parent, int[] rank) {
-    int parentOfI = find(i, parent);
-    int parentOfJ = find(j, parent);
-    if (parentOfI == parentOfJ) {
-      return;
-    }
-    if (rank[parentOfI] > rank[parentOfJ]) {
-      parent[parentOfJ] = parentOfI;
-    } else if (rank[parentOfI] < rank[parentOfJ]) {
-      parent[parentOfI] = parentOfJ;
-    } else {
-      parent[parentOfI] = parentOfJ;
-      rank[parentOfJ]++;
-    }
-  }
-
-  private int removeStones(int[][] stones) {
-    int n = stones.length;
-    int[] parent = new int[n];
-    int[] rank = new int[n];
-    for (int i = 0; i < n; i++) {
-      parent[i] = i;
-    }
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
-          union(i, j, parent, rank);
+    public int find(int i, int[] parent) {
+        if (i == parent[i]) {
+            return i;
         }
-      }
+        return parent[i] = find(parent[i], parent);
     }
-    int groups = 0;
-    for (int i = 0; i < n; i++) {
-      if (parent[i] == i) {
-        groups++;
-      }
+
+    public void union(int i, int j, int[] parent, int[] rank) {
+        int parentOfI = find(i, parent);
+        int parentOfJ = find(j, parent);
+        if (parentOfI == parentOfJ) {
+            return;
+        }
+        if (rank[parentOfI] > rank[parentOfJ]) {
+            parent[parentOfJ] = parentOfI;
+        } else if (rank[parentOfI] < rank[parentOfJ]) {
+            parent[parentOfI] = parentOfJ;
+        } else {
+            parent[parentOfI] = parentOfJ;
+            rank[parentOfJ]++;
+        }
     }
-    return n - groups;
-  }
+
+    private int removeStones(int[][] stones) {
+        int n = stones.length;
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    union(i, j, parent, rank);
+                }
+            }
+        }
+        int totalGroups = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                totalGroups++;
+            }
+        }
+        return n - totalGroups;
+    }
 }
