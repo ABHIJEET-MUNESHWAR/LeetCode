bb06e43c065214db790f9a37413b1ce4068d6b9f
Abhijeet Muneshwar <openingknots@gmail.com>
2024-09-01 11:43:52 +0530

    :art:         int size = original.length;
            return result;

--- src/com/leetcode/easy/array/Construct2DArray.java
+++ src/com/leetcode/easy/array/Construct2DArray.java
@@ -15,10 +15,42 @@ public class Construct2DArray {
         Construct2DArray construct2DArray = new Construct2DArray();
         int[] original = {1, 2, 3, 4};
         int m = 2, n = 2;
-        ArrayUtils.printMatrix(construct2DArray.construct2DArray(original, m, n));
+        ArrayUtils.printMatrix(construct2DArray.construct2DArrayOne(original, m, n));
+        ArrayUtils.printMatrix(construct2DArray.construct2DArrayTwo(original, m, n));
+        ArrayUtils.printMatrix(construct2DArray.construct2DArrayThree(original, m, n));
     }
 
-    private int[][] construct2DArray(int[] original, int m, int n) {
+    private int[][] construct2DArrayThree(int[] original, int m, int n) {
+        int size = original.length;
+        if (size != m * n) {
+            return new int[0][0];
+        }
+        int[][] result = new int[m][n];
+        int index = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = original[index];
+                index++;
+            }
+        }
+        return result;
+    }
+
+    private int[][] construct2DArrayTwo(int[] original, int m, int n) {
+        int size = original.length;
+        if (size != m * n) {
+            return new int[0][0];
+        }
+        int[][] result = new int[m][n];
+        for (int i = 0; i < size; i++) {
+            int row = i / n;
+            int col = i % n;
+            result[row][col] = original[i];
+        }
+        return result;
+    }
+
+    private int[][] construct2DArrayOne(int[] original, int m, int n) {
         int size = original.length;
         if (size != m * n) {
             return new int[0][0];
