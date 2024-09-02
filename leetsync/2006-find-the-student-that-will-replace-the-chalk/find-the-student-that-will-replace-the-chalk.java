f56edd905a74c57308a157c3e8dd0b1dcfbf2948
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-09-02 22:03:18 +0530

    Time: 2 ms (72.59%) | Memory: 60.7 MB (6.23%) - LeetSync

--- leetsync/2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ leetsync/2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -1,6 +1,5 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-
         int n = chalk.length;
         long totalChalkSum = 0;
         // Calculate the total sum of chalk required in one iteration
