3909e30e6b411d77800ff2cc1e8ba3c3eadc01e1
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-09-24 22:22:33 +0530

    Time: 35 ms (88.69%) | Memory: 55.6 MB (70.41%) - LeetSync

--- leetsync/3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ leetsync/3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,22 +1,51 @@
-class Solution {
-    public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        Set<Integer> set = new HashSet<>();
+class TrieNode {
+    TrieNode[] children = new TrieNode[10]; // 0, 1, 2...9
+}
 
-        // Insert all prefixes of elements from arr1 into the set
-        for (int num : arr1) {
-            while (!set.contains(num) & num > 0) {
-                set.add(num);
-                num = num / 10; // Trim last digit
+class Solution {
+    public TrieNode getTrieNode() {
+        return new TrieNode();
+    }
+
+    public void insert(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numStr = Integer.toString(num);
+        for (char ch : numStr.toCharArray()) {
+            int index = ch - '0';
+            if (crawl.children[index] == null) {
+                crawl.children[index] = getTrieNode();
+            }
+            crawl = crawl.children[index];
+        }
+    }
+
+    public int search(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numStr = Integer.toString(num);
+        int length = 0;
+        for (char ch : numStr.toCharArray()) {
+            int index = ch - '0';
+            if (crawl.children[index] != null) {
+                length++;
+                crawl = crawl.children[index];
+            } else {
+                break;
             }
         }
-        int result = 0; // LCP
+        return length;
+    }
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        TrieNode root = getTrieNode(); // Trie
+
+        // Insert all elements of arr1 into the trie
+        for (int num : arr1) {
+            insert(num, root);
+        }
+        int result = 0;
+        // Search for the longest common prefix in arr2
         for (int num : arr2) {
-            while (!set.contains(num) & num > 0) {
-                num = num / 10; // Trim last digit
-            }
-            if (num > 0) {
-                result = Math.max(result, (int) Math.log10(num) + 1);
-            }
+            result = Math.max(result, search(num, root));
         }
         return result;
     }
