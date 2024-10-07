ef426760bf5abdae2a1207bf6019cab7a6ddb41b
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-10-07 12:18:55 +0530

    Time: 2 ms (23.11%) | Memory: 42 MB (7.97%) - LeetSync

--- leetsync/1923-sentence-similarity-iii/sentence-similarity-iii.java
+++ leetsync/1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -1,36 +1,23 @@
 class Solution {
 
-    // Helper function to split a sentence into words
-    private String[] splitWords(String sentence) {
-        return sentence.split(" ");
-    }
+    public boolean areSentencesSimilar(String s1, String s2) {
+        if (s1.length() < s2.length()) {
+            String temp = s2;
+            s2 = s1;
+            s1 = temp;
+        }
+        Deque<String> dq1 = new LinkedList<>(Arrays.asList(s1.split(" ")));
+        Deque<String> dq2 = new LinkedList<>(Arrays.asList(s2.split(" ")));
 
-    public boolean areSentencesSimilar(String sentence1, String sentence2) {
-        // Split both sentences into arrays of words
-        String[] words1 = splitWords(sentence1);
-        String[] words2 = splitWords(sentence2);
-
-        // Ensure words1 is the longer sentence
-        if (words1.length < words2.length) {
-            String[] temp = words1;
-            words1 = words2;
-            words2 = temp;
+        while (!dq1.isEmpty() && !dq2.isEmpty() && dq1.peekFirst().equals(dq2.peekFirst())) {
+            dq1.pollFirst();
+            dq2.pollFirst();
         }
 
-        int start = 0, end = 0;
-        int n1 = words1.length, n2 = words2.length;
-
-        // Compare from the start
-        while (start < n2 && words1[start].equals(words2[start])) {
-            start++;
+        while (!dq1.isEmpty() && !dq2.isEmpty() && dq1.peekLast().equals(dq2.peekLast())) {
+            dq1.pollLast();
+            dq2.pollLast();
         }
-
-        // Compare from the end
-        while (end < n2 && words1[n1 - end - 1].equals(words2[n2 - end - 1])) {
-            end++;
-        }
-
-        // Check if the remaining unmatched part is in the middle
-        return start + end >= n2;
+        return dq2.isEmpty();
     }
 }
