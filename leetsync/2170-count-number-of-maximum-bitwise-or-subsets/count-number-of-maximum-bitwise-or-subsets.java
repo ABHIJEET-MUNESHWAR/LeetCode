72e8cb0d5a5b306c61fe10adf7adf567f91dcfc8
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-10-18 21:21:07 +0530

    Time: 72 ms (31.43%) | Memory: 65.2 MB (5.26%) - LeetSync

--- leetsync/2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ leetsync/2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int countSubSets(int index, int currentOr, int maxOr, int[] nums) {
+    public int countSubSets(int index, int currentOr, int maxOr, int[] nums, int[][] dp) {
         if (index == nums.length) {
             if (currentOr == maxOr) {
-                return 1; // Found one subset
+                return dp[index][currentOr] = 1; // Found one subset
             }
-            return 0;
+            return dp[index][currentOr] = 0;
+        }
+
+        if (dp[index][currentOr] != -1) {
+            return dp[index][currentOr];
         }
 
         // Take nums[idx]
-        int takeCount = countSubSets(index + 1, currentOr | nums[index], maxOr, nums);
+        int takeCount = countSubSets(index + 1, currentOr | nums[index], maxOr, nums, dp);
 
         // Not taken nums[idx]
-        int skipCount = countSubSets(index + 1, currentOr, maxOr, nums);
+        int skipCount = countSubSets(index + 1, currentOr, maxOr, nums, dp);
 
-        return takeCount + skipCount;
+        return dp[index][currentOr] = (takeCount + skipCount);
     }
 
     public int countMaxOrSubsets(int[] nums) {
@@ -22,6 +26,13 @@ class Solution {
             maxOr |= num;
         }
         int currentOr = 0;
-        return countSubSets(0, currentOr, maxOr, nums);
+        int n = nums.length;
+        int[][] dp = new int[n + 1][maxOr + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= maxOr; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return countSubSets(0, currentOr, maxOr, nums, dp);
     }
 }
