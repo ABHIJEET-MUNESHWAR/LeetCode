e54c1d5f9bf4ea5c24d042d6a00d9592e9b79e9a
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-11-27 21:10:06 +0530

    Time: 218 ms (34.78%) | Memory: 45.9 MB (7.97%) - LeetSync

--- leetsync/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ leetsync/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,43 +1,50 @@
 class Solution {
-    private Map<Integer, List<Integer>> adj = new HashMap<>();
+    private Map<Integer, List<int[]>> adj = new HashMap<>();
 
-    public int bfs(int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n];
-        queue.add(0);
-        int level = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
-                int node = queue.poll();
-                if (node == n - 1) {
-                    return level;// Found the destination node
-                }
-                for (int neighbour : adj.getOrDefault(node, new ArrayList<>())) {
-                    if (!visited[neighbour]) {
-                        queue.add(neighbour);
-                        visited[neighbour] = true;
-                    }
+    public int dijkstra(int n) {
+        int[] result = new int[n];
+        Arrays.fill(result, Integer.MAX_VALUE);
+        result[0] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[] { 0, 0 }); // distance, node
+        while (!pq.isEmpty()) {
+            int[] top = pq.poll();
+            int d = top[0];
+            int node = top[1];
+            if (node == n - 1) {
+                return result[n - 1];
+            }
+            if (d > result[node]) {
+                continue;
+            }
+            if (!adj.containsKey(node)) {
+                continue;
+            }
+            for (int[] edge : adj.get(node)) {
+                int adjNode = edge[0];
+                int dist = edge[1];
+                if ((dist + d) < result[adjNode]) {
+                    result[adjNode] = dist + d;
+                    pq.add(new int[] { d + dist, adjNode });
                 }
             }
-            level++;
         }
-        return -1;// If destination node is unreachable
+        return result[n - 1];
     }
 
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
         adj.clear();
         // Initialize adjacency list with direct edges (i -> i + 1)
-        for (int i = 0; i < n; i++) {
-            adj.computeIfAbsent(i, k -> new ArrayList<>()).add(i + 1);
+        for (int i = 0; i < n - 1; i++) {
+            adj.computeIfAbsent(i, k -> new ArrayList<>()).add(new int[] { i + 1, 1 });
         }
         int k = queries.length;
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
             int u = queries[i][0];
             int v = queries[i][1];
-            adj.computeIfAbsent(u, k1 -> new ArrayList<>()).add(v);
-            result[i] = bfs(n);
+            adj.computeIfAbsent(u, k1 -> new ArrayList<>()).add(new int[] { v, 1 });
+            result[i] = dijkstra(n);
         }
         return result;
     }
