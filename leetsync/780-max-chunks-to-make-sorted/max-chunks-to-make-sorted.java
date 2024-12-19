6e25b5b4b9abcd6e085117ae899f6b3530b1a9cf
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-19 22:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (62.41%) - LeetSync

---
+++ leetsync/780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int chunksCount=0;
+        int cumSum=0, normalSum=0;
+        for(int i=0; i<n; i++){
+            cumSum+=arr[i];
+            normalSum+=i;
+            if(cumSum==normalSum){
+                chunksCount++;
+            }
+        }
+        return chunksCount;
+    }
+}
