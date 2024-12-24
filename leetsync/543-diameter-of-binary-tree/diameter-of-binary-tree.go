8398ba06a3f8d2a0d7eac1bc531ff69c742352c6
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-24 21:00:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (41.52%) - LeetSync

---
+++ leetsync/543-diameter-of-binary-tree/diameter-of-binary-tree.go
@@ -0,0 +1,25 @@
+var maxD int
+
+func diameterOfBinaryTree(root *TreeNode) int {
+    maxD = 0
+    find(root)
+    return maxD
+}
+
+func find(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    left := find(root.Left)
+    right := find(root.Right)
+    localMax := left + right
+    maxD = max(maxD, localMax)
+    return max(left, right) + 1
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
