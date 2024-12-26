4e52035215b0838fcd85b8cf524688481911321d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-26 18:35:40 +0530

    Time: 631 ms (21.22%) | Memory: 41.2 MB (80.82%) - LeetSync

--- leetsync/494-target-sum/target-sum.java
+++ leetsync/494-target-sum/target-sum.java
@@ -1,31 +1,20 @@
 class Solution {
+    public int solve(int[] nums, int target, int index, int currentSum) {
+        if (index == nums.length) {
+            if (currentSum == target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
 
-    public int findTargetSumWays(int[] nums, int target) {
-        int total = 0;
-        for (int x : nums) {
-            total += x;
-        }
-        int[][] dp = new int[nums.length + 1][2 * total + 1];
-        for (int i = 0; i < nums.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return solve(nums, target, 0, 0, 0, dp, total);
+        int plus = solve(nums, target, index + 1, currentSum + nums[index]);
+        int minus = solve(nums, target, index + 1, currentSum - nums[index]);
+        return plus + minus;
     }
 
-    private int solve(int[] nums, int target, int index, int currentTotal, int count, int[][] dp, int totalSum) {
-        if (index >= nums.length && currentTotal == target) {
-            return ++count;
-        }
-        if (index >= nums.length) {
-            return 0;
-        }
-        if (dp[index][currentTotal + totalSum] != -1) {
-            return dp[index][currentTotal + totalSum];
-        }
-
-        dp[index][currentTotal + totalSum] = solve(nums, target, index + 1, currentTotal + nums[index], count, dp,
-                totalSum)
-                + solve(nums, target, index + 1, currentTotal - nums[index], count, dp, totalSum);
-        return dp[index][currentTotal + totalSum];
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        return solve(nums, target, 0, 0);
     }
 }
