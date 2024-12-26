7d7059a9d3dc8f5b2958cda8f6952002df6f70be
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-26 19:08:51 +0530

    Time: 428 ms (29.41%) | Memory: 114.8 MB (5.04%) - LeetSync

--- leetsync/494-target-sum/target-sum.go
+++ leetsync/494-target-sum/target-sum.go
@@ -1,8 +1,21 @@
+var totalSum = 0
+
 func findTargetSumWays(nums []int, target int) int {
-	return solve(nums, target, 0, 0)
+	for _, val := range nums {
+		totalSum += val
+	}
+	memo := make([][]int, len(nums))
+	for index := range memo {
+		memo[index] = make([]int, totalSum*2+1)
+		for index2 := range memo[index] {
+			memo[index][index2] = -1
+		}
+	}
+	currentSum := 0
+	return solve(nums, target, 0, currentSum, &memo)
 }
 
-func solve(nums []int, target int, index int, currentSum int) int {
+func solve(nums []int, target int, index int, currentSum int, memo *[][]int) int {
 	if index == len(nums) {
 		if currentSum == target {
 			return 1
@@ -10,7 +23,11 @@ func solve(nums []int, target int, index int, currentSum int) int {
 			return 0
 		}
 	}
-	plus := solve(nums, target, index+1, currentSum+nums[index])
-	minus := solve(nums, target, index+1, currentSum-nums[index])
-	return plus + minus
+	if (*memo)[index][totalSum+currentSum] != -1 {
+		return (*memo)[index][totalSum+currentSum]
+	}
+	plus := solve(nums, target, index+1, currentSum+nums[index], memo)
+	minus := solve(nums, target, index+1, currentSum-nums[index], memo)
+	(*memo)[index][totalSum+currentSum] = plus + minus
+	return (*memo)[index][totalSum+currentSum]
 }
