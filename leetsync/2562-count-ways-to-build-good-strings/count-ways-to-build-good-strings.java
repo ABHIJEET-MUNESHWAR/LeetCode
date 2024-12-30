0a2673941fb8648f0eec30b6768a1d5e9eb6e144
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-30 13:26:36 +0530

    Time: 29 ms (7.72%) | Memory: 55.4 MB (20.47%) - LeetSync

--- leetsync/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ leetsync/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -1,22 +1,26 @@
 class Solution {
     int mod = 1000000007;
+    int[] dp;
 
     public int countGoodStrings(int low, int high, int zero, int one) {
-        int[] dp = new int[high + 1];
-        dp[0] = 1;
-        int md = 1000000007;
-        for (int i = Math.min(zero, one); i <= high; i++) {
-            if (i >= zero) {
-                dp[i] = (dp[i] + dp[i - zero]) % md;
-            }
-            if (i >= one) {
-                dp[i] = (dp[i] + dp[i - one]) % md;
-            }
+        dp = new int[1000001];
+        Arrays.fill(dp, -1);
+        return solveRecursionMemoization(low, high, zero, one, 0);
+    }
+
+    private int solveRecursionMemoization(int low, int high, int zero, int one, int currentStringSize) {
+        if (currentStringSize > high) {
+            return 0;
         }
-        int sum = 0;
-        for (int i = low; i <= high; i++) {
-            sum = (sum + dp[i]) % md;
+        if (dp[currentStringSize] != -1) {
+            return dp[currentStringSize];
         }
-        return sum;
+        int addOne = 0;
+        if (currentStringSize >= low && currentStringSize <= high) {
+            addOne = 1;
+        }
+        int appendZero = solveRecursionMemoization(low, high, zero, one, currentStringSize + zero);
+        int appendOne = solveRecursionMemoization(low, high, zero, one, currentStringSize + one);
+        return dp[currentStringSize] = (addOne + appendZero + appendOne) % mod;
     }
 }
