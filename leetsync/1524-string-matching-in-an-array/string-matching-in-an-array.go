ee9df5b2ab6d1c21b68f59dacae686f96471d244
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-01-08 11:46:05 +0530

    Time: 5 ms (0.00%) | Memory: 8.4 MB (8.70%) - LeetSync

--- leetsync/1524-string-matching-in-an-array/string-matching-in-an-array.go
+++ leetsync/1524-string-matching-in-an-array/string-matching-in-an-array.go
@@ -1,20 +1,58 @@
 func stringMatching(words []string) []string {
-	n := len(words)
-	wordSet := make(map[string]struct{})
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
+	result := []string{}
+
+	for i, pattern := range words {
+		for j, text := range words {
 			if i == j {
 				continue
 			}
-			if strings.Contains(words[i], words[j]) {
-				wordSet[words[j]] = struct{}{}
+			if pattern == text || KMPSearch(text, pattern) {
+				result = append(result, pattern)
+				break
 			}
 		}
 	}
-	var result []string
 
-	for k, _ := range wordSet {
-		result = append(result, k)
-	}
 	return result
+}
+
+func KMPSearch(text, pattern string) bool {
+	pi := computePrefixFunction(pattern)
+	j := 0
+
+	for i := 0; i < len(text); i++ {
+		for j > 0 && pattern[j] != text[i] {
+			j = pi[j-1]
+		}
+		if text[i] == pattern[j] {
+			j++
+		}
+		if j == len(pattern) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func computePrefixFunction(pattern string) []int {
+	if len(pattern) == 0 {
+		return []int{}
+	}
+	pi := make([]int, len(pattern))
+	pi[0] = 0
+
+	j := 0
+	for i := 1; i < len(pattern); i++ {
+		for j > 0 && pattern[i] != pattern[j] {
+			j = pi[j-1]
+		}
+
+		if pattern[i] == pattern[j] {
+			j++
+		}
+		pi[i] = j
+	}
+
+	return pi
 }
