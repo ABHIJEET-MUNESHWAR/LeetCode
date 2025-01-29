f106149441db86a464305a18374ecde7f220bf83
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-01-29 10:22:27 +0530

    Time: 1 ms (75.21%) | Memory: 43.2 MB (75.11%) - LeetSync

--- leetsync/684-redundant-connection/redundant-connection.java
+++ leetsync/684-redundant-connection/redundant-connection.java
@@ -1,42 +1,52 @@
-class Solution {
+class DSU {
+    private int[] rank;
+    private int[] parent;
 
-    public int find(int i, int[] parent) {
+    public DSU(int n) {
+        rank = new int[n + 1];
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int i) {
         if (parent[i] == i) {
             return i;
         }
-        return parent[i] = find(parent[i], parent);
+        return parent[i] = find(parent[i]);
     }
 
-    public void union(int i, int j, int[] parent, int[] rank) {
-        int parentOfI = find(i, parent);
-        int parentOfJ = find(j, parent);
+    public void union(int i, int j) {
+        int parentOfI = find(i);
+        int parentOfJ = find(j);
         if (parentOfI == parentOfJ) {
             return;
         }
         if (rank[parentOfI] > rank[parentOfJ]) {
-            parent[parentOfJ] = parentOfI;
-        } else if (rank[parentOfI] < rank[parentOfJ]) {
-            parent[parentOfI] = parentOfJ;
+            parent[parentOfJ] = parent[parentOfI];
+        } else if (rank[parentOfI] < parent[parentOfJ]) {
+            parent[parentOfI] = parent[parentOfJ];
         } else {
-            parent[parentOfI] = parentOfJ;
-            rank[parentOfI]++;
+            parent[parentOfI] = parent[parentOfJ];
+            rank[parentOfJ]++;
         }
     }
+}
+
+class Solution {
 
     public int[] findRedundantConnection(int[][] edges) {
-        int size = edges.length;
-        int[] parent = new int[size + 1];
-        int[] rank = new int[size + 1];
-        for (int i = 1; i <= size; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
+        int m = edges.length;
+        DSU dsu = new DSU(m);
         for (int[] edge : edges) {
-            if (find(edge[0], parent) == find(edge[1], parent)) {
+            int i = edge[0];
+            int j = edge[1];
+            if (dsu.find(i) == dsu.find(j)) {
                 return edge;
             }
-            union(edge[0], edge[1], parent, rank);
+            dsu.union(i, j);
         }
-        return new int[] { -1, -1 };
+        return new int[0];
     }
 }
