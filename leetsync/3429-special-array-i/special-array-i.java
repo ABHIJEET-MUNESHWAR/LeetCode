d3737df35dd45633fc7ed484c9d49d909d870110
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-02-01 14:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (8.78%) - LeetSync

--- leetsync/3429-special-array-i/special-array-i.java
+++ leetsync/3429-special-array-i/special-array-i.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         int n = nums.length;
-        boolean isEven = nums[0] % 2 == 0;
-        for (int i = 1; i < n; i++) {
-            isEven = !isEven;
-            boolean currentParity = nums[i] % 2 == 0;
-            if (currentParity != isEven) {
+
+        for (int i = 0; i < n - 1; i++) {
+
+            if (nums[i] % 2 == nums[i + 1] % 2) {
                 return false;
             }
         }
