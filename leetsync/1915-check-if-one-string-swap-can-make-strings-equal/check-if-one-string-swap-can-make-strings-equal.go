c6d26b033ecf4ae3305bfdd00b67a9857d8cf4c9
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-02-05 20:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 3.9 MB (100.00%) - LeetSync

--- leetsync/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.go
+++ leetsync/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.go
@@ -1,24 +1,21 @@
 func areAlmostEqual(s1 string, s2 string) bool {
 	n := len(s1)
 	diffCount := 0
+	firstMismatch, secondMismatch := 0, 0
 	for i := 0; i < n; i++ {
 		if s1[i] != s2[i] {
 			diffCount++
-		}
-		if diffCount > 2 {
-			return false
-		}
-	}
-	first := make([]int, 26)
-	second := make([]int, 26)
-	for i := 0; i < n; i++ {
-		first[s1[i]-'a']++
-		second[s2[i]-'a']++
-	}
-	for i := 0; i < 26; i++ {
-		if first[i] != second[i] {
-			return false
+			if diffCount > 2 {
+				return false
+			} else if diffCount == 1 {
+				firstMismatch = i
+			} else if diffCount == 2 {
+				secondMismatch = i
+			}
 		}
 	}
-	return true
+	if s1[firstMismatch] == s2[secondMismatch] && s1[secondMismatch] == s2[firstMismatch] {
+		return true
+	}
+	return false
 }
