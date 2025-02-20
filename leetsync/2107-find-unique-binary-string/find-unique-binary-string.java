687ef88c2c278b99ccdec909c671a2205a76ac67
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-02-20 10:44:16 +0530

    Time: 1 ms (67.36%) | Memory: 41.6 MB (57.18%) - LeetSync

--- leetsync/2107-find-unique-binary-string/find-unique-binary-string.java
+++ leetsync/2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,40 +1,14 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        List<String> allSubSequences = new ArrayList<>();
-        String current = new String();
-        generateAllSubSequences(nums[0].length(), allSubSequences, current, 0);
-        Set<String> set = new HashSet<>();
-        for (String num : nums) {
-            set.add(num);
-        }
-        for (String subSequence : allSubSequences) {
-            if (!set.contains(subSequence)) {
-                return subSequence;
+        String answer = "";
+        for (int i = 0; i < nums.length; i++) {
+            char ch = nums[i].charAt(i);
+            if (ch == '1') {
+                answer += '0';
+            } else {
+                answer += '1';
             }
         }
-        return "";
-    }
-
-    public void generateAllSubSequences(int length, List<String> allSubSequences, String current, int index) {
-        if (index >= length) {
-            allSubSequences.add(current);
-            return;
-        }
-
-        // Backtrack with 0
-        // Do
-        current = current + "0";
-        // Explore
-        generateAllSubSequences(length, allSubSequences, current, index + 1);
-        // Undo
-        current = current.substring(0, current.length() - 1);
-
-        // Backtrack with 1
-        // Do
-        current = current + "1";
-        // Explore
-        generateAllSubSequences(length, allSubSequences, current, index + 1);
-        // Undo
-        current = current.substring(0, current.length() - 1);
+        return answer;
     }
 }
