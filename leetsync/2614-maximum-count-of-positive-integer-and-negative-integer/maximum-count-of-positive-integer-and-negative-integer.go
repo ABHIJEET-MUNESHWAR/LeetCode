db51050c64e1f04031a3e4571d639708e3fc2106
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-03-12 10:22:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.15%) - LeetSync

--- leetsync/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.go
+++ leetsync/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.go
@@ -1,11 +1,20 @@
 func maximumCount(nums []int) int {
-	positives, negatives := 0, 0
-	for _, num := range nums {
-		if num > 0 {
-			positives++
-		} else if num < 0 {
-			negatives++
+	positives, negatives := lowerBound(nums, 1), lowerBound(nums, 0)
+	n := len(nums)
+	return max(n-positives, negatives)
+}
+func lowerBound(nums []int, target int) int {
+	n := len(nums)
+	left, right := 0, n-1
+	result := n
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			result = mid
+			right = mid - 1
 		}
 	}
-	return max(positives, negatives)
+	return result
 }
