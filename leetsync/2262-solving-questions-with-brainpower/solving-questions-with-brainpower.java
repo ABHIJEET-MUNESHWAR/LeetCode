985dceb65a8f65f1a3e475fe3ea84c78850bc536
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-04-01 16:55:36 +0530

    Time: 7 ms (53.04%) | Memory: 78.6 MB (99.46%) - LeetSync

--- leetsync/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ leetsync/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,20 +1,13 @@
 class Solution {
-    public long solve(int[][] q, int index, long[] dp) {
-        if (index >= q.length) {
-            return 0;
-        }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-        long take = q[index][0] + solve(q, index + q[index][1] + 1, dp);
-        long skip = solve(q, index + 1, dp);
-        return dp[index] = Math.max(take, skip);
-    }
-
     public long mostPoints(int[][] q) {
         int n = q.length;
-        long[] dp = new long[n];
-        Arrays.fill(dp, -1);
-        return solve(q, 0, dp);
+        long[] dp = new long[200001];
+        if (n == 1) {
+            return q[0][0];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Math.max(dp[i + 1], q[i][0] + dp[q[i][1] + i + 1]);
+        }
+        return dp[0];
     }
 }
