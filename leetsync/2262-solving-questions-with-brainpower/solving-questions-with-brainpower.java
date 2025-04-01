79f47a719c2288c9d9ecf6ea49d9f223a75cdce9
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-04-01 16:34:51 +0530

    Time: 10 ms (42.86%) | Memory: 125.4 MB (21.25%) - LeetSync

--- leetsync/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ leetsync/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,22 +1,20 @@
 class Solution {
-
-    long[] dp;
-
-    public long mostPoints(int[][] questions) {
-        dp = new long[questions.length + 1];
-        Arrays.fill(dp, -1L);
-        return solveRecursionMemoization(questions, 0);
-    }
-
-    private long solveRecursionMemoization(int[][] questions, int index) {
-        if (index >= questions.length) {
+    public long solve(int[][] q, int index, long[] dp) {
+        if (index >= q.length) {
             return 0;
         }
         if (dp[index] != -1) {
             return dp[index];
         }
-        long take = questions[index][0] + solveRecursionMemoization(questions, index + questions[index][1] + 1);
-        long skip = solveRecursionMemoization(questions, index + 1);
+        long take = q[index][0] + solve(q, index + q[index][1] + 1, dp);
+        long skip = solve(q, index + 1, dp);
         return dp[index] = Math.max(take, skip);
+    }
+
+    public long mostPoints(int[][] q) {
+        int n = q.length;
+        long[] dp = new long[n];
+        Arrays.fill(dp, -1);
+        return solve(q, 0, dp);
     }
 }
