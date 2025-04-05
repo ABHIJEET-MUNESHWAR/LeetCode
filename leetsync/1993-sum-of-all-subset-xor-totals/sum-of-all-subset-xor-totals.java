6fb61e83d6359c9fcd8fb78b71868449baac7ee4
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-04-05 23:00:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (63.91%) - LeetSync

--- leetsync/1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ leetsync/1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,15 +1,10 @@
 class Solution {
-
-    public int solve(int[] nums, int index, int xor) {
-        if (index >= nums.length) {
-            return xor;
-        }
-        int take = solve(nums, index + 1, nums[index] ^ xor);
-        int skip = solve(nums, index + 1, xor);
-        return take + skip;
-    }
-
     public int subsetXORSum(int[] nums) {
-        return solve(nums, 0, 0);
+        int n = nums.length;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result |= nums[i];
+        }
+        return result << (n - 1);
     }
 }
