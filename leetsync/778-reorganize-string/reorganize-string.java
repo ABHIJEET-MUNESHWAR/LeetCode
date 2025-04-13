938fa92907a14218d1af1a298a573c7a60ad95b6
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-04-13 10:22:05 +0530

    Time: 1 ms (92.66%) | Memory: 41.8 MB (60.70%) - LeetSync

--- leetsync/778-reorganize-string/reorganize-string.java
+++ leetsync/778-reorganize-string/reorganize-string.java
@@ -1,39 +1,37 @@
 class Solution {
     public String reorganizeString(String s) {
-        Map<Character, Integer> freqToCharMap = new HashMap<>();
         int n = s.length();
-        for (char ch : s.toCharArray()) {
-            int freq = freqToCharMap.getOrDefault(ch, 0) + 1;
-            if (freq > ((n + 1) / 2)) {
+        char[] result = new char[n];
+        int[] freq = new int[26];
+        int maxOccFreq = 0;
+        char maxOccChar = '$';
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            freq[ch - 'a']++;
+            if (freq[ch - 'a'] > ((n + 1) / 2)) {
                 return "";
             }
-            freqToCharMap.put(ch, freq);
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for (Map.Entry<Character, Integer> entry : freqToCharMap.entrySet()) {
-            char ch = entry.getKey();
-            int freq = entry.getValue();
-            pq.add(new int[] { freq, ch });
-        }
-        StringBuilder sb = new StringBuilder();
-        while (pq.size() >= 2) {
-            int[] first = pq.poll();
-            int[] second = pq.poll();
-            sb.append((char) first[1]);
-            sb.append((char) second[1]);
-            first[0]--;
-            second[0]--;
-            if (first[0] > 0) {
-                pq.add(first);
-            }
-            if (second[0] > 0) {
-                pq.add(second);
+            if (maxOccFreq < freq[ch - 'a']) {
+                maxOccFreq = freq[ch - 'a'];
+                maxOccChar = ch;
             }
         }
-        if (!pq.isEmpty()) {
-            int[] first = pq.poll();
-            sb.append((char) first[1]);
+        int j = 0;
+        while (maxOccFreq-- > 0) {
+            result[j] = maxOccChar;
+            j += 2;
+            freq[maxOccChar - 'a']--;
         }
-        return sb.toString();
+        for (char i = 'a'; i <= 'z'; i++) {
+            while (freq[i - 'a'] > 0) {
+                if (j > n - 1) {
+                    j = 1;
+                }
+                result[j] = i;
+                j += 2;
+                freq[i - 'a']--;
+            }
+        }
+        return new String(result);
     }
 }
