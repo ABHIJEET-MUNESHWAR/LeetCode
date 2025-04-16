c292845d458c920ddf0b90b18d0baeb030a8e98f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-04-16 17:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (67.69%) - LeetSync

---
+++ leetsync/50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double solve(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return solve(1 / x, -n);
+        }
+        if (n % 2 == 0) {
+            return solve(x * x, n / 2);
+        }
+        return x * solve(x * x, (n - 1) / 2);
+    }
+
+    public double myPow(double x, int n) {
+        return solve(x, (long) n);
+    }
+}
