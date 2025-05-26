ab13b2f9a0adcdcc175dd4d18141e00437fcfe80
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-05-26 22:31:22 +0530

    Time: 120 ms (36.16%) | Memory: 56.9 MB (85.45%) - LeetSync

--- leetsync/2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ leetsync/2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,18 +1,25 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        Map<String, Integer> nonPaired = new HashMap<>();
-        int pairs = 0, sym = 0;
-        for (String w : words) {
-            String reverse = new StringBuilder(w).reverse().toString();
-            if (nonPaired.getOrDefault(reverse, 0) > 0) { // Find a counterpart for w among non-paired words.
-                ++pairs;                                  // Increase the counter by 1.
-                nonPaired.merge(reverse, -1, Integer::sum); // Decrease reverse by 1 since it has been counted in pairs.
-                sym -= w.charAt(0) == w.charAt(1) ? 1 : 0; // Decrease sym by 1 since it has been counted in pairs.
+        Map<String, Integer> wordToFreqMap = new HashMap<>();
+        int result = 0;
+        for (String word : words) {
+            String reverseWord = new StringBuilder(word).reverse().toString();
+            if (wordToFreqMap.getOrDefault(reverseWord, 0) > 0) {
+                result += 4;
+                wordToFreqMap.put(reverseWord, wordToFreqMap.get(reverseWord) - 1);
             } else {
-                nonPaired.merge(w, 1, Integer::sum); // Increase the occurrence of w.
-                sym += w.charAt(0) == w.charAt(1) ? 1 : 0; // Increase sym by 1.
+                wordToFreqMap.put(word, wordToFreqMap.getOrDefault(word, 0) + 1);
             }
         }
-        return 4 * pairs + (sym > 0 ? 2 : 0);
+        // Check for a word with both characters same to use in the center
+        for (Map.Entry<String, Integer> entry : wordToFreqMap.entrySet()) {
+            String word = entry.getKey();
+            int freq = entry.getValue();
+            if (word.charAt(0) == word.charAt(1) && freq > 0) {
+                result += 2;
+                break;
+            }
+        }
+        return result;
     }
 }
