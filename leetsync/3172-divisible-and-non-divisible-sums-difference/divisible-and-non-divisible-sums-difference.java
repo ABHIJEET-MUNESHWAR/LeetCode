c53acfa4ad839c7044c4de1b317a62826eb925e5
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-05-27 11:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (28.90%) - LeetSync

--- leetsync/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ leetsync/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -1,6 +1,6 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int k = n/m;
-        return n*(n+1)/2-m*k*(k+1);
+        int k = n / m;
+        return n * (n + 1) / 2 - m * k * (k + 1);
     }
 }
