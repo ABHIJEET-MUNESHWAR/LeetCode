1fb0abe43b5af384e60f1cddccd91993a89b1cf8
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-08-13 21:37:59 +0530

    Time: 8 ms (84.41%) | Memory: 44.4 MB (18.53%) - LeetSync

--- leetsync/326-power-of-three/power-of-three.java
+++ leetsync/326-power-of-three/power-of-three.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        return n > 0 && 1162261467 % n == 0;
+        if (n <= 0) {
+            return false;
+        }
+        double logValue = Math.log10(n) / Math.log10(3);
+        // Check if logValue is an integer
+        return logValue == (int) logValue;
     }
 }
