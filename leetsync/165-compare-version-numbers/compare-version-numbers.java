c3e7f6167315177d42e6a0277947a6e84c9dc586
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-09-23 21:13:31 +0530

    Time: 1 ms (76.03%) | Memory: 41.9 MB (11.52%) - LeetSync

--- leetsync/165-compare-version-numbers/compare-version-numbers.java
+++ leetsync/165-compare-version-numbers/compare-version-numbers.java
@@ -1,15 +1,19 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-
-        String[] levels1 = version1.split("\\.");
-        String[] levels2 = version2.split("\\.");
-        int length = Math.max(levels1.length, levels2.length);
-        for (int i = 0; i < length; i++) {
-            Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
-            Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
-            int compare = v1.compareTo(v2);
-            if (compare != 0) {
-                return compare;
+        String[] s1 = version1.split("\\.");
+        String[] s2 = version2.split("\\.");
+        int m = s1.length;
+        int n = s2.length;
+        int i = 0;
+        while (i < m || i < n) {
+            int a = i < m ? Integer.parseInt(s1[i]) : 0;
+            int b = i < n ? Integer.parseInt(s2[i]) : 0;
+            if (a > b) {
+                return 1;
+            } else if (b > a) {
+                return -1;
+            } else {
+                i++;
             }
         }
         return 0;
