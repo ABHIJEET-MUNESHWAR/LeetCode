c4f62e33926ac8948d6cac3ca204212e7faca93b
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-04-21 20:09:42 +0530

    Time: 38 ms (93.55%) | Memory: 111.5 MB (90.32%) - LeetSync

---
+++ leetsync/1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.java
@@ -0,0 +1,45 @@
+class Solution {
+    private int[] parent;
+
+    private int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    private void unite(int a, int b) {
+        parent[find(a)] = find(b);
+    }
+
+    public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+        int n = source.length;
+        parent = new int[n];
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        for (int[] swap : allowedSwaps) {
+            unite(swap[0], swap[1]);
+        }
+
+        // Group source values by their component root
+        Map<Integer, Map<Integer, Integer>> groups = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int root = find(i);
+            groups.computeIfAbsent(root, k -> new HashMap<>())
+                    .merge(source[i], 1, Integer::sum);
+        }
+
+        int hammingDist = 0;
+        for (int i = 0; i < n; i++) {
+            int root = find(i);
+            Map<Integer, Integer> freq = groups.get(root);
+            if (freq.getOrDefault(target[i], 0) > 0) {
+                freq.merge(target[i], -1, Integer::sum); // matched, consume this source value
+            } else {
+                hammingDist++; // no match found in this component
+            }
+        }
+
+        return hammingDist;
+    }
+}
