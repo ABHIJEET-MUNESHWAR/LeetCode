44268b1514e5928795174984526ec23d6d15cc08
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-21 10:33:43 +0530

    Time: 94 ms (69.01%) | Memory: 48.1 MB (39.44%) - LeetSync

---
+++ leetsync/3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int ones = 0;
+        for (char c : s.toCharArray())
+            if (c == '1')
+                ones++;
+
+        s = "1" + s + "1";
+
+        int n = s.length();
+        int i = 0;
+
+        int ans = ones;
+
+        // Skip first 1's
+        while (i < n && s.charAt(i) == '1')
+            i++;
+
+        // Read first 0-block
+        int c10 = 0;
+        while (i < n && s.charAt(i) == '0') {
+            c10++;
+            i++;
+        }
+
+        while (i < n) {
+
+            // Read middle 1-block
+            int c11 = 0;
+            while (i < n && s.charAt(i) == '1') {
+                c11++;
+                i++;
+            }
+
+            if (c11 == 0)
+                break;
+
+            // Read right 0-block
+            int c20 = 0;
+            while (i < n && s.charAt(i) == '0') {
+                c20++;
+                i++;
+            }
+
+            if (c20 == 0)
+                break;
+
+            ans = Math.max(ans, ones + c10 + c20);
+
+            // Slide the window
+            c10 = c20;
+        }
+
+        return ans;
+    }
+}
