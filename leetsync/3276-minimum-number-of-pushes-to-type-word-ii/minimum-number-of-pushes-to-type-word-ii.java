24dba299f6faf64bd21057d9fd80b30332ff0c40
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-31 12:06:17 +0530

    Time: 16 ms (24.86%) | Memory: 48 MB (73.70%) - LeetSync

--- leetsync/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ leetsync/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,22 +1,18 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] letterFrequency = new int[26];
-        for (char c : word.toCharArray()) {
-            letterFrequency[c - 'a']++;
+        int n = word.length();
+        int result = 0;
+        int[] letterToFrequencyMap = new int[26];
+        for (int i = 0; i < n; i++) {
+            letterToFrequencyMap[word.charAt(i) - 'a']++;
         }
-        
-        Integer[] sortedFreq = new Integer[26];
+        Integer[] mpInteger = Arrays.stream(letterToFrequencyMap).boxed().toArray(Integer[]::new);
+        Arrays.sort(mpInteger, Comparator.reverseOrder());
         for (int i = 0; i < 26; i++) {
-            sortedFreq[i] = letterFrequency[i];
+            int frequency = mpInteger[i];
+            int press = (i / 8) + 1;
+            result += frequency * press;
         }
-        Arrays.sort(sortedFreq, Collections.reverseOrder());
-        
-        int totalPresses = 0;
-        for (int i = 0; i < 26; i++) {
-            if (sortedFreq[i] == 0) break;
-            totalPresses += (i / 8 + 1) * sortedFreq[i];
-        }
-        
-        return totalPresses;
+        return result;
     }
 }
