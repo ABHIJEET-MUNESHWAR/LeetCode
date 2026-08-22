bde387940391704ed5961175f39e91f07e1e6523
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-22 18:04:40 +0530

    Time: 383 ms (5.04%) | Memory: 48.1 MB (8.65%) - LeetSync

--- leetsync/55-jump-game/jump-game.java
+++ leetsync/55-jump-game/jump-game.java
@@ -1,13 +1,22 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int size = nums.length;
-        int maxReachable = 0;
-        for (int i = 0; i < size; i++) {
-            if (i > maxReachable) {
-                return false;
-            }
-            maxReachable = Math.max(maxReachable, nums[i] + i);
+    public boolean solveCanJump(int[] nums, int n, int index, Boolean[] dp) {
+        if (index == n - 1) {
+            return true;
         }
-        return true;
+        if (dp[index] != null) {
+            return dp[index];
+        }
+        for (int i = 1; i <= nums[index]; i++) {
+            if (solveCanJump(nums, n, index + i, dp)) {
+                return dp[index] = true;
+            }
+        }
+        return dp[index] = false;
+    }
+
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        Boolean[] dp = new Boolean[n];
+        return solveCanJump(nums, n, 0, dp);
     }
 }
