d67781a7249870a3a132e682886a9fb5cae485fe
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-22 18:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (32.50%) - LeetSync

--- leetsync/199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ leetsync/199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,38 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-
-    List<Integer> list = new ArrayList<>();
-
-    public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) {
-            return list;
-        }
-        rightSideView(root, 1);
-        return list;
-    }
-
-    private void rightSideView(TreeNode root, int currentLevel) {
+    public void rightSideView(TreeNode root, List<Integer> result, int level) {
         if (root == null) {
             return;
         }
-        if (list.size() < currentLevel) {
-            list.add(root.val);
+        if (result.size() < level) {
+            result.add(root.val);
         }
-        rightSideView(root.right, currentLevel + 1);
-        rightSideView(root.left, currentLevel + 1);
+        rightSideView(root.right, result, level + 1);
+        rightSideView(root.left, result, level + 1);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        rightSideView(root, result, 1);
+        return result;
     }
 }
