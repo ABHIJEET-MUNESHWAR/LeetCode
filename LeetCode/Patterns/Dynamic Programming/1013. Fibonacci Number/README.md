3d2b81def38b8439ba572c9e6fe2b36aee0053d9
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 15:10:10 +0530

    Add Easy Java Solution for 1013. Fibonacci Number from LeetCode

--- LeetCode/Patterns/Dynamic Programming/1013. Fibonacci Number/README.md
+++ LeetCode/Patterns/Dynamic Programming/1013. Fibonacci Number/README.md
@@ -8,8 +8,8 @@
 Math, Dynamic Programming, Recursion, Memoization
 
 ### 🚀 Performance
-- **Runtime:** 9 ms
-- **Memory:** 41.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Dynamic Programming/1013. Fibonacci Number/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/1013. Fibonacci Number/Solution.java
@@ -1,11 +1,17 @@
 class Solution {
+    public int topDownFib(int n, int[] dp) {
+        if (n <= 1) {
+            return n;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = topDownFib(n - 1, dp) + topDownFib(n - 2, dp);
+    }
+
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
-        return fib(n - 1) + fib(n - 2);
+        int[] dp = new int[31];
+        Arrays.fill(dp, -1);
+        return topDownFib(n, dp);
     }
 }
