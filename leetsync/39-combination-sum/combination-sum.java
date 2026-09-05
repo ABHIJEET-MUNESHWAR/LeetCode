51a5ace557a2ff092f0aa393257f82ea4070d3d8
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 16:12:22 +0530

    Time: 2 ms (95.23%) | Memory: 46 MB (9.58%) - LeetSync

--- leetsync/39-combination-sum/combination-sum.java
+++ leetsync/39-combination-sum/combination-sum.java
@@ -1,25 +1,25 @@
 class Solution {
+    public void combinationSumBackTrack(int[] nums, int target, int n, int index, List<Integer> currentList,
+            List<List<Integer>> resultList) {
+        if (target == 0) {
+            resultList.add(new ArrayList<>(currentList));
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        for (int i = index; i < n; i++) {
+            currentList.add(nums[i]);
+            combinationSumBackTrack(nums, target - nums[i], n, i, currentList, resultList);
+            currentList.remove(currentList.size() - 1);
+        }
+    }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> resultList = new ArrayList<>();
         List<Integer> currentList = new ArrayList<>();
         int n = candidates.length;
         combinationSumBackTrack(candidates, target, n, 0, currentList, resultList);
         return resultList;
-    }
-
-    public void combinationSumBackTrack(int[] candidates, int remainingTarget, int n, int index,
-            List<Integer> currentList, List<List<Integer>> resultList) {
-        if (remainingTarget == 0) {
-            resultList.add(new ArrayList(currentList));
-            return;
-        }
-        if (remainingTarget < 0 || index >= n) {
-            return;
-        }
-        for (int j = index; j < n; j++) {
-            currentList.add(candidates[j]);
-            combinationSumBackTrack(candidates, remainingTarget - candidates[j], n, j, currentList, resultList);
-            currentList.remove(currentList.size() - 1);
-        }
     }
 }
