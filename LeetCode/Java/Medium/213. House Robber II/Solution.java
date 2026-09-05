53b404ed9528df06ec94f5005a74194130ddc996
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 22:01:01 +0530

    Add Medium Java Solution for 213. House Robber II from LeetCode

--- LeetCode/Java/Medium/213. House Robber II/Solution.java
+++ LeetCode/Java/Medium/213. House Robber II/Solution.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
+    public int robRecursion(int[] nums, int n, int index, int[] dp) {
+        if (index > n) {
+            return 0;
         }
-        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        int take = nums[index] + robRecursion(nums, n, index + 2, dp);
+        int skip = robRecursion(nums, n, index + 1, dp);
+        return dp[index] = Math.max(take, skip);
     }
 
-    private int rob(int[] nums, int start, int end) {
-        int prevPrev = 0;
-        int prev = 0;
-
-        for (int i = start; i <= end; i++) {
-            int steal = nums[i] + prevPrev;
-            int skip = prev;
-            int temp = Math.max(steal, skip);
-            prevPrev = prev;
-            prev = temp;
-        }
-
-        return prev;
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[101];
+        Arrays.fill(dp, -1);
+        int take0thIndexHouse = robRecursion(nums, n - 2, 0, dp);
+        Arrays.fill(dp, -1);
+        int take1stIndexHouse = robRecursion(nums, n - 1, 1, dp);
+        return Math.max(take0thIndexHouse, take1stIndexHouse);
     }
 }
