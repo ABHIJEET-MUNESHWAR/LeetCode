9b8e5de1825cb70d57e9d658213de715e42a65aa
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 15:07:21 +0530

    Add Easy Java Solution for 1013. Fibonacci Number from LeetCode

--- LeetCode/Java/Easy/1013. Fibonacci Number/Solution.java
+++ LeetCode/Java/Easy/1013. Fibonacci Number/Solution.java
@@ -1,21 +1,11 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) {
-            return n;
+        if (n == 0) {
+            return 0;
         }
-        int[] dp = new int[31];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
-    }
-
-    private int solve(int n, int[] dp) {
-        if (n <= 1) {
-            return n;
+        if (n == 1) {
+            return 1;
         }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-        return dp[n];
+        return fib(n - 1) + fib(n - 2);
     }
 }
