21e1e90409f7887b5759be093ee51cbf9c0cbb91
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 21:23:55 +0530

    Add Medium Java Solution for 210. Course Schedule II from LeetCode

--- LeetCode/Patterns/Depth-first search/210. Course Schedule II/Solution.java
+++ LeetCode/Patterns/Depth-first search/210. Course Schedule II/Solution.java
@@ -1,6 +1,9 @@
 class Solution {
-    public void isCycleDfs(int numCourses, Map<Integer, List<Integer>> adj, int u, boolean[] isVisited,
-            boolean[] inRecursion, Stack<Integer> stack, boolean hasCycle) {
+    boolean hasCycle = false;
+
+    public void hasCycleDFS(Map<Integer, List<Integer>> adj, int numCourses, boolean[] isVisited, boolean[] inRecursion,
+            int u,
+            Stack<Integer> stack) {
         isVisited[u] = true;
         inRecursion[u] = true;
         for (int v : adj.getOrDefault(u, new ArrayList<>())) {
@@ -9,36 +12,35 @@ class Solution {
                 return;
             }
             if (!isVisited[v]) {
-                isCycleDfs(numCourses, adj, v, isVisited, inRecursion, stack, hasCycle);
+                hasCycleDFS(adj, numCourses, isVisited, inRecursion, v, stack);
             }
         }
+        stack.add(u);
         inRecursion[u] = false;
-        stack.push(u);
     }
 
-    public int[] findOrder(int numCourses, int[][] edges) {
-        Stack<Integer> stack = new Stack<>();
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         boolean[] isVisited = new boolean[numCourses];
         boolean[] inRecursion = new boolean[numCourses];
         Map<Integer, List<Integer>> adj = new HashMap<>();
-        for (int[] edge : edges) {
+        Stack<Integer> stack = new Stack<>();
+        for (int[] edge : prerequisites) {
             int u = edge[0];
             int v = edge[1];
             adj.computeIfAbsent(v, key -> new ArrayList<>()).add(u);
         }
-        boolean hasCycle = false;
         for (int i = 0; i < numCourses; i++) {
             if (!isVisited[i]) {
-                isCycleDfs(numCourses, adj, i, isVisited, inRecursion, stack, hasCycle);
+                hasCycleDFS(adj, numCourses, isVisited, inRecursion, i, stack);
             }
         }
         if (hasCycle) {
             return new int[] {};
         }
         int[] result = new int[stack.size()];
-        int itr = 0;
+        int i = 0;
         while (!stack.isEmpty()) {
-            result[itr++] = stack.pop();
+            result[i++] = stack.pop();
         }
         return result;
     }
