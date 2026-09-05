6630e56532e7c5157987cda087ca467f3f426099
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 19:00:54 +0530

    Add Medium Java Solution for 40. Combination Sum II from LeetCode

--- LeetCode/Java/Medium/40. Combination Sum II/Solution.java
+++ LeetCode/Java/Medium/40. Combination Sum II/Solution.java
@@ -1,28 +1,29 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        Arrays.sort(candidates);
-        solve(candidates, target, result, current, 0);
-        return result;
-    }
-
-    private void solve(int[] candidates, int target, List<List<Integer>> result, List<Integer> current, int index) {
+    public void combinationSum2BackTrack(int[] nums, int target, int n, int index, List<Integer> currentList,
+            List<List<Integer>> resultList) {
+        if (target == 0) {
+            resultList.add(new ArrayList<>(currentList));
+            return;
+        }
         if (target < 0) {
             return;
         }
-        if (target == 0) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-        for (int i = index; i < candidates.length; i++) {
-            if (i > index && candidates[i] == candidates[i - 1]) {
-                // Ignore duplicate elements
+        for (int i = index; i < n; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
                 continue;
             }
-            current.add(candidates[i]);
-            solve(candidates, target - candidates[i], result, current, i + 1);
-            current.remove(current.size() - 1);
+            currentList.add(nums[i]);
+            combinationSum2BackTrack(nums, target - nums[i], n, i + 1, currentList, resultList);
+            currentList.remove(currentList.size() - 1);
         }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        List<Integer> currentList = new ArrayList<>();
+        int n = candidates.length;
+        Arrays.sort(candidates);
+        combinationSum2BackTrack(candidates, target, n, 0, currentList, resultList);
+        return resultList;
     }
 }
