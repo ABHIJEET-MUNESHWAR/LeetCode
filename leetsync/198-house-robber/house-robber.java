f2f071bd0013765c71af2305ab426bbb1148a2fb
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 20:36:21 +0530

    Time: 1 ms (1.94%) | Memory: 43 MB (19.17%) - LeetSync

--- leetsync/198-house-robber/house-robber.java
+++ leetsync/198-house-robber/house-robber.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[401];
-        Arrays.fill(dp, -1);
-        return solve(nums, n, 0, dp);
-    }
-
-    public int solve(int[] nums, int n, int index, int[] dp) {
+    public int robRecursion(int[] nums, int n, int index, int[] dp) {
         if (index >= n) {
             return 0;
         }
         if (dp[index] != -1) {
             return dp[index];
         }
-        int rob = nums[index] + solve(nums, n, index + 2, dp);
-        int skip = solve(nums, n, index + 1, dp);
-        return dp[index] = Math.max(rob, skip);
+        int take = nums[index] + robRecursion(nums, n, index + 2, dp);
+        int skip = robRecursion(nums, n, index + 1, dp);
+        return dp[index] = Math.max(take, skip);
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[401];
+        Arrays.fill(dp, -1);
+        return robRecursion(nums, n, 0, dp);
     }
 }
