1ef2f604bee596f0eb0509a1dbdc78c0d12664c4
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 18:36:54 +0530

    Add Easy Java Solution for 70. Climbing Stairs from LeetCode

--- LeetCode/Java/Easy/70. Climbing Stairs/Solution.java
+++ LeetCode/Java/Easy/70. Climbing Stairs/Solution.java
@@ -1,15 +1,11 @@
 class Solution {
-    public int climbStairsRecursion(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        if (n < 0) {
-            return 0;
-        }
-        return climbStairsRecursion(n - 1) + climbStairsRecursion(n - 2);
-    }
-
     public int climbStairs(int n) {
-        return climbStairsRecursion(n);
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
--- LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/README.md
+++ LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/README.md
@@ -8,8 +8,8 @@
 Math, Dynamic Programming, Memoization
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 0 ms
+- **Memory:** 40.6 MB
 
 ---
 
