6294c7519263954bad6a93b0c9710c39ba72d40f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 18:38:11 +0530

    Add Easy Java Solution for 70. Climbing Stairs from LeetCode

--- LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/Solution.java
@@ -1,11 +1,20 @@
 class Solution {
-    public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+    public int climbStairsRecursion(int n, int[] dp) {
+        if (n == 0) {
+            return 1;
         }
-        return dp[n];
+        if (n < 0) {
+            return 0;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = climbStairsRecursion(n - 1, dp) + climbStairsRecursion(n - 2, dp);
+    }
+
+    public int climbStairs(int n) {
+        int[] dp = new int[46];
+        Arrays.fill(dp, -1);
+        return climbStairsRecursion(n, dp);
     }
 }
