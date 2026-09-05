d9a145df86056310e7683fac2a340743ec5997a0
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 18:25:57 +0530

    Add Medium Java Solution for 207. Course Schedule from LeetCode

--- LeetCode/Java/Medium/207. Course Schedule/Solution.java
+++ LeetCode/Java/Medium/207. Course Schedule/Solution.java
@@ -1,10 +1,9 @@
 class Solution {
-    public boolean isCycleDFS(Map<Integer, List<Integer>> adj, int numCourses, boolean[] isVisited,
-            boolean[] inRecursion, int u) {
+    public boolean isCycleDfs(Map<Integer, List<Integer>> adj, boolean[] isVisited, boolean[] inRecursion, int u) {
         isVisited[u] = true;
         inRecursion[u] = true;
         for (int v : adj.getOrDefault(u, new ArrayList<>())) {
-            if (!isVisited[v] && isCycleDFS(adj, numCourses, isVisited, inRecursion, v)) {
+            if (!isVisited[v] && isCycleDfs(adj, isVisited, inRecursion, v)) {
                 return true;
             } else if (inRecursion[v]) {
                 return true;
@@ -15,16 +14,16 @@ class Solution {
     }
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean[] isVisited = new boolean[numCourses];
-        boolean[] inRecursion = new boolean[numCourses];
         Map<Integer, List<Integer>> adj = new HashMap<>();
-        for (int[] edge : prerequisites) {
-            int u = edge[0];
-            int v = edge[1];
+        for (int[] prerequisite : prerequisites) {
+            int u = prerequisite[0];
+            int v = prerequisite[1];
             adj.computeIfAbsent(v, key -> new ArrayList<>()).add(u);
         }
+        boolean[] isVisited = new boolean[numCourses];
+        boolean[] inRecursion = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!isVisited[i] && isCycleDFS(adj, numCourses, isVisited, inRecursion, i)) {
+            if (!isVisited[i] && isCycleDfs(adj, isVisited, inRecursion, i)) {
                 return false;
             }
         }
