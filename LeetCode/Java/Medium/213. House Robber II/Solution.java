a85d5639c0ccdbbeb0fd950008da70ecff42266f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-05 21:58:49 +0530

    Add Medium Java Solution for 213. House Robber II from LeetCode

--- LeetCode/Java/Medium/213. House Robber II/Solution.java
+++ LeetCode/Java/Medium/213. House Robber II/Solution.java
@@ -1,30 +1,23 @@
 class Solution {
-    int[] dp = new int[101];
-
     public int rob(int[] nums) {
-        int size = nums.length;
-        if (size == 1) {
+        if (nums.length == 1) {
             return nums[0];
         }
-        if (size == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-        Arrays.fill(dp, -1);
-        int take0thIndexHouse = solve(nums, 0, size - 2);
-        Arrays.fill(dp, -1);
-        int take1stIndexHouse = solve(nums, 1, size - 1);
-        return Math.max(take0thIndexHouse, take1stIndexHouse);
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
     }
 
-    private int solve(int[] nums, int index, int size) {
-        if (index > size) {
-            return 0;
+    private int rob(int[] nums, int start, int end) {
+        int prevPrev = 0;
+        int prev = 0;
+
+        for (int i = start; i <= end; i++) {
+            int steal = nums[i] + prevPrev;
+            int skip = prev;
+            int temp = Math.max(steal, skip);
+            prevPrev = prev;
+            prev = temp;
         }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-        int steal = nums[index] + solve(nums, index + 2, size);
-        int skip = solve(nums, index + 1, size);
-        return Math.max(steal, skip);
+
+        return prev;
     }
 }
