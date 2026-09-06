4dec1e4f780916c7d91d0d54dd8227ec66f24ac0
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-06 19:59:40 +0530

    Add Medium Java Solution for 300. Longest Increasing Subsequence from LeetCode

--- LeetCode/Java/Medium/300. Longest Increasing Subsequence/Solution.java
+++ LeetCode/Java/Medium/300. Longest Increasing Subsequence/Solution.java
@@ -1,28 +1,20 @@
 class Solution {
+    int size;
 
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
+        size = nums.length;
+        return solve(nums, 0, -1);
+    }
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(nums[0]);
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > arr.get(arr.size() - 1)) {
-                arr.add(nums[i]);
-            } else {
-                int l = 0, r = arr.size() - 1, ans = 0;
-                while (l <= r) {
-                    int mid = (l + r) / 2;
-
-                    if (nums[i] <= arr.get(mid)) {
-                        ans = mid;
-                        r = mid - 1;
-                    } else {
-                        l = mid + 1;
-                    }
-                }
-                arr.set(ans, nums[i]);
-            }
+    private int solve(int[] nums, int index, int previous) {
+        if (index >= size) {
+            return 0;
         }
-        return arr.size();
+        int take = 0;
+        if (previous == -1 || nums[previous] < nums[index]) {
+            take = 1 + solve(nums, index + 1, index);
+        }
+        int skip = solve(nums, index + 1, previous);
+        return Math.max(take, skip);
     }
 }
