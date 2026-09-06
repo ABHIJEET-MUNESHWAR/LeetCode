a06e702bf1a9d0072e3158e994d0f7a8bb1b5edb
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-06 20:21:06 +0530

    Add Medium Java Solution for 62. Unique Paths from LeetCode

--- LeetCode/Java/Medium/62. Unique Paths/Solution.java
+++ LeetCode/Java/Medium/62. Unique Paths/Solution.java
@@ -1,15 +1,22 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
-            }
+    public int uniquePathsRecursionMemoization(int m, int n, int i, int j, int[][]dp) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
         }
-        return dp[m - 1][n - 1];
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        return dp[i][j]=uniquePathsRecursionMemoization(m, n, i + 1, j, dp) + uniquePathsRecursionMemoization(m, n, i, j + 1, dp);
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][]dp = new int[m+1][n+1];
+        for(int i=0; i<m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return uniquePathsRecursionMemoization(m, n, 0, 0, dp);
     }
 }
