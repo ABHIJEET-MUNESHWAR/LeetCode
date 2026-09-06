0afa7963519bdbb2e10e11c941c363ff45f993c5
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-06 21:58:30 +0530

    Add Medium Java Solution for 1036. Rotting Oranges from LeetCode

--- LeetCode/Java/Medium/1036. Rotting Oranges/Solution.java
+++ LeetCode/Java/Medium/1036. Rotting Oranges/Solution.java
@@ -1,41 +1,42 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<int[]> queue = new LinkedList<>();
         int m = grid.length;
         int n = grid[0].length;
-        int[][] directions = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-        Queue<int[]> queue = new LinkedList<>();
+        int freshOranges = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
                     queue.offer(new int[] { i, j });
+                } else if (grid[i][j] == 1) {
+                    freshOranges++;
                 }
             }
         }
+        if (freshOranges == 0) {
+            return 0;
+        }
         int minutes = 0;
+        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
         while (!queue.isEmpty()) {
-            int queueSize = queue.size();
-            while (queueSize-- > 0) {
-                int[] orangeLocation = queue.poll();
-                int i = orangeLocation[0];
-                int j = orangeLocation[1];
-                for (int[] direction : directions) {
-                    int i_ = i + direction[0];
-                    int j_ = j + direction[1];
-                    if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && grid[i_][j_] == 1) {
-                        grid[i_][j_] = 2;
-                        queue.add(new int[] { i_, j_ });
+            int N = queue.size();
+            while (N-- > 0) {
+                int[] currentCell = queue.poll();
+                int i = currentCell[0];
+                int j = currentCell[1];
+
+                for (int[] dir : directions) {
+                    int new_i = i + dir[0];
+                    int new_j = j + dir[1];
+                    if (new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && grid[new_i][new_j] == 1) {
+                        queue.add(new int[] { new_i, new_j });
+                        grid[new_i][new_j] = 2;
+                        freshOranges--;
                     }
                 }
             }
             minutes++;
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    return -1;
-                }
-            }
-        }
-        return minutes - 1;
+        return freshOranges == 0 ? minutes - 1 : -1;
     }
 }
