5a0f7eb61f6cd99fd9be4f450e3a7c50e670889d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-06 15:56:49 +0530

    Add Medium Java Solution for 2022. Maximum Alternating Subsequence Sum from LeetCode

--- LeetCode/Java/Medium/2022. Maximum Alternating Subsequence Sum/Solution.java
+++ LeetCode/Java/Medium/2022. Maximum Alternating Subsequence Sum/Solution.java
@@ -1,13 +1,25 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        int size = nums.length;
-        long[][] dp = new long[size + 1][2];
-        dp[0][0] = Math.max(-nums[0], 0);
-        dp[0][1] = Math.max(nums[0], 0);
-        for (int i = 1; i < size; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - nums[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + nums[i]);
+        long[][] dp = new long[nums.length + 1][2]; // in each row we save the value and its flag value
+        for (int i = 0; i <= nums.length; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return Math.max(dp[size - 1][0], dp[size - 1][1]);
+        return solveRecursionMemoization(nums, 0, 1, dp);
+    }
+
+    private long solveRecursionMemoization(int[] nums, int index, int isEven, long[][] dp) {
+        if (index >= nums.length) {
+            return 0;
+        }
+        if (dp[index][isEven] != -1) {
+            return dp[index][isEven];
+        }
+        long skip = solveRecursionMemoization(nums, index + 1, isEven, dp);
+        int value = nums[index];
+        if (isEven == 0) {
+            value = -value;
+        }
+        long take = solveRecursionMemoization(nums, index + 1, 1 - isEven, dp) + value;
+        return dp[index][isEven] = Math.max(take, skip);
     }
 }
