409bcf19850a244f45525a1a92a0244708d731c5
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-06 20:07:44 +0530

    Add Medium Java Solution for 300. Longest Increasing Subsequence from LeetCode

--- LeetCode/Patterns/Dynamic Programming/300. Longest Increasing Subsequence/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/300. Longest Increasing Subsequence/Solution.java
@@ -1,28 +1,28 @@
 class Solution {
+    public int lengthOfLISRecursionMemoization(int[] nums, int n, int currentIndex, int previousIndex, int[][] dp) {
+        if (currentIndex >= n) {
+            return 0;
+        }
+        if (previousIndex != -1 && dp[currentIndex][previousIndex] != -1) {
+            return dp[currentIndex][previousIndex];
+        }
+        int take = 0;
+        if (previousIndex == -1 || nums[previousIndex] < nums[currentIndex]) {
+            take = 1 + lengthOfLISRecursionMemoization(nums, n, currentIndex + 1, currentIndex, dp);
+        }
+        int skip = lengthOfLISRecursionMemoization(nums, n, currentIndex + 1, previousIndex, dp);
+        if (previousIndex != -1) {
+            dp[currentIndex][previousIndex] = Math.max(take, skip);
+        }
+        return Math.max(take, skip);
+    }
 
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(nums[0]);
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > arr.get(arr.size() - 1)) {
-                arr.add(nums[i]);
-            } else {
-                int l = 0, r = arr.size() - 1, ans = 0;
-                while (l <= r) {
-                    int mid = (l + r) / 2;
-
-                    if (nums[i] <= arr.get(mid)) {
-                        ans = mid;
-                        r = mid - 1;
-                    } else {
-                        l = mid + 1;
-                    }
-                }
-                arr.set(ans, nums[i]);
-            }
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return arr.size();
+        return lengthOfLISRecursionMemoization(nums, n, 0, -1, dp);
     }
 }
