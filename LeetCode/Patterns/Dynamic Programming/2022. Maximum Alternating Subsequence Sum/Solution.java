afb49c64644816d77d1ed9d0b91df829e90685f3
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-06 15:56:18 +0530

    Add Medium Java Solution for 2022. Maximum Alternating Subsequence Sum from LeetCode

--- LeetCode/Patterns/Dynamic Programming/2022. Maximum Alternating Subsequence Sum/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/2022. Maximum Alternating Subsequence Sum/Solution.java
@@ -1,13 +1,18 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        int size = nums.length;
-        long[][] dp = new long[size + 1][2];
-        dp[0][0] = Math.max(-nums[0], 0);
-        dp[0][1] = Math.max(nums[0], 0);
-        for (int i = 1; i < size; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - nums[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + nums[i]);
+        return solve(nums, 0, true);
+    }
+
+    private int solve(int[] nums, int index, boolean isEven) {
+        if (index >= nums.length) {
+            return 0;
         }
-        return Math.max(dp[size - 1][0], dp[size - 1][1]);
+        int skip = solve(nums, index + 1, isEven);
+        int value = nums[index];
+        if (!isEven) {
+            value = -value;
+        }
+        int take = solve(nums, index + 1, !isEven) + value;
+        return Math.max(take, skip);
     }
 }
