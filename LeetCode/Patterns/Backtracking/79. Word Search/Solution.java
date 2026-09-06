e3187a9acf9026d0faad4674c1fcb78c780ae033
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-06 16:28:15 +0530

    Add Medium Java Solution for 79. Word Search from LeetCode

--- LeetCode/Patterns/Backtracking/79. Word Search/Solution.java
+++ LeetCode/Patterns/Backtracking/79. Word Search/Solution.java
@@ -1,5 +1,24 @@
 class Solution {
-    int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    public boolean existBackTrackDfs(char[][] board, int i, int j, String word, int wordIndex, boolean[][] isVisited) {
+        if (wordIndex == word.length() - 1) {
+            return true;
+        }
+        isVisited[i][j] = true;
+        int m = board.length;
+        int n = board[0].length;
+        int[][] directions = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        for (int[] direction : directions) {
+            int i_ = i + direction[0];
+            int j_ = j + direction[1];
+            if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && board[i_][j_] == word.charAt(wordIndex + 1)) {
+                if (!isVisited[i_][j_] && existBackTrackDfs(board, i_, j_, word, wordIndex + 1, isVisited)) {
+                    return true;
+                }
+            }
+        }
+        isVisited[i][j] = false;
+        return false;
+    }
 
     public boolean exist(char[][] board, String word) {
         int m = board.length;
@@ -7,34 +26,12 @@ class Solution {
         boolean[][] isVisited = new boolean[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (wordSearchDFS(board, word, 0, i, j, isVisited)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean wordSearchDFS(char[][] board, String word, int index, int i, int j, boolean[][] isVisited) {
-        int m = board.length;
-        int n = board[0].length;
-        if (index == word.length() - 1) {
-            return true;
-        }
-        isVisited[i][j] = true;
-        for (int[] direction : directions) {
-            int i_ = i + direction[0];
-            int j_ = j + direction[1];
-            if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && !isVisited[i_][j_]
-                    && board[i_][j_] == word.charAt(index + 1)) {
-                if (wordSearchDFS(board, word, index + 1, i_, j_, isVisited)) {
+                if (!isVisited[i][j] && word.charAt(0) == board[i][j]
+                        && existBackTrackDfs(board, i, j, word, 0, isVisited)) {
                     return true;
                 }
             }
         }
-        isVisited[i][j] = false;
         return false;
     }
 }
