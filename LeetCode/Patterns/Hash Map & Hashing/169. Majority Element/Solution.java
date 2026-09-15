7dfc323123b21df0622f72dcba9641254a87db41
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-15 16:43:24 +0530

    Add Easy Java Solution for 169. Majority Element from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/169. Majority Element/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/169. Majority Element/Solution.java
@@ -1,19 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-      int len = nums.length;
-      int count = 0;
-      int num = 0;
-      for (int i = 0; i < len; i++) {
-        if (count == 0) {
-          num = nums[i];
+        int n = nums.length;
+        int count = 0;
+        int num = nums[0];
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                num = nums[i];
+            }
+            if (num != nums[i]) {
+                count--;
+            } else {
+                count++;
+            }
         }
-        if (num != nums[i]) {
-          count--;
-        } else {
-          count++;
-        }
-      }
-      return num;
-  }
+        return num;
+    }
 }
