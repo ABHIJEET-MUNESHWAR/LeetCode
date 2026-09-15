ed2e7affc3968a5030976e62f77a44c72529c2fc
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-15 15:35:14 +0530

    Add Medium Java Solution for 433. Minimum Genetic Mutation from LeetCode

--- LeetCode/Java/Medium/433. Minimum Genetic Mutation/README.md
+++ LeetCode/Java/Medium/433. Minimum Genetic Mutation/README.md
@@ -8,8 +8,8 @@
 Hash Table, String, Breadth-First Search, Bidirectional Search
 
 ### 🚀 Performance
-- **Runtime:** 1 ms
-- **Memory:** 41.3 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Medium/433. Minimum Genetic Mutation/Solution.java
+++ LeetCode/Java/Medium/433. Minimum Genetic Mutation/Solution.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
+
         Set<String> bankGeneSet = new HashSet<>();
         for (String bankGene : bank) {
             bankGeneSet.add(bankGene);
@@ -12,22 +13,19 @@ class Solution {
         int levels = 0;
         int geneSize = startGene.length();
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
-                String currentGene = queue.poll();
-                if (currentGene.equals(endGene)) {
-                    return levels;
-                }
-                for (char choice : choices) {
-                    for (int i = 0; i < geneSize; i++) {
-                        char[] currentGeneCharArray = currentGene.toCharArray();
-                        if (currentGeneCharArray[i] != choice) {
-                            currentGeneCharArray[i] = choice;
-                            String mutatedGene = new String(currentGeneCharArray);
-                            if (!visitedGeneSet.contains(mutatedGene) && bankGeneSet.contains(mutatedGene)) {
-                                visitedGeneSet.add(mutatedGene);
-                                queue.add(mutatedGene);
-                            }
+            String currentGene = queue.poll();
+            if (currentGene.equals(endGene)) {
+                return levels;
+            }
+            for (char choice : choices) {
+                for (int i = 0; i < geneSize; i++) {
+                    char[] currentGeneCharArray = currentGene.toCharArray();
+                    if (currentGeneCharArray[i] != choice) {
+                        currentGeneCharArray[i] = choice;
+                        String mutatedGene = new String(currentGeneCharArray);
+                        if (!visitedGeneSet.contains(mutatedGene) && bankGeneSet.contains(mutatedGene)) {
+                            visitedGeneSet.add(mutatedGene);
+                            queue.add(mutatedGene);
                         }
                     }
                 }
