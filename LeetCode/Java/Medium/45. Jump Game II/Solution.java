8fdee8168f4b9aa275548b592908f30a6ff0ce4e
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-15 17:46:28 +0530

    Add Medium Java Solution for 45. Jump Game II from LeetCode

--- LeetCode/Java/Medium/45. Jump Game II/Solution.java
+++ LeetCode/Java/Medium/45. Jump Game II/Solution.java
@@ -1,22 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return canJumpRecursionMemoization(nums, nums.length, 0, memo);
+        return canJumpRecursion(nums, nums.length, 0);
     }
 
-    private int canJumpRecursionMemoization(int[] nums, int size, int currentIndex, int[] memo) {
+    private int canJumpRecursion(int[] nums, int size, int currentIndex) {
         if (currentIndex == size - 1) {
             return 0;
         }
-        if (memo[currentIndex] != -1) {
-            return memo[currentIndex];
-        }
         int minJump = 99999;
+        int jump = nums[currentIndex];
         for (int i = currentIndex + 1; (i <= currentIndex + nums[currentIndex] && i < size); i++) {
-            int value = 1 + canJumpRecursionMemoization(nums, size, i, memo);
+            int value = 1 + canJumpRecursion(nums, size, i);
             minJump = Math.min(minJump, value);
         }
-        return memo[currentIndex] = minJump;
+        return minJump;
     }
 }
