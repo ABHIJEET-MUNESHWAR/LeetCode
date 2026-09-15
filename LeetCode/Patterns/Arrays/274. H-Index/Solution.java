2b539205834823445b6bae70185c097cd9034283
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-15 22:02:40 +0530

    Add Medium Java Solution for 274. H-Index from LeetCode

---
+++ LeetCode/Patterns/Arrays/274. H-Index/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (citations[mid] == (n - mid)) {
+                return citations[mid];
+            } else if (citations[mid] > (n - mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return n - low;
+    }
+}
