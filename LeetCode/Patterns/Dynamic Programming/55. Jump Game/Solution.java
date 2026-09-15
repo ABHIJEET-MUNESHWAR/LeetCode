931ba7058abce266eaf7bc993c8a14ae7d90e6de
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-15 17:42:59 +0530

    Add Medium Java Solution for 55. Jump Game from LeetCode

--- LeetCode/Patterns/Dynamic Programming/55. Jump Game/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/55. Jump Game/Solution.java
@@ -1,13 +1,16 @@
 class Solution {
-    public boolean solveCanJump(int[] nums, int n, int index, Boolean[] dp) {
-        if (index == n - 1) {
+    public boolean canJumpRecursion(int[] nums, int n, int index, Boolean[] dp) {
+        if (index == (n - 1)) {
             return true;
+        }
+        if (index >= n) {
+            return false;
         }
         if (dp[index] != null) {
             return dp[index];
         }
         for (int i = 1; i <= nums[index]; i++) {
-            if (solveCanJump(nums, n, index + i, dp)) {
+            if (canJumpRecursion(nums, n, index + i, dp)) {
                 return dp[index] = true;
             }
         }
@@ -16,7 +19,7 @@ class Solution {
 
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        Boolean[] dp = new Boolean[n];
-        return solveCanJump(nums, n, 0, dp);
+        Boolean[] dp = new Boolean[n + 1];
+        return canJumpRecursion(nums, n, 0, dp);
     }
 }
