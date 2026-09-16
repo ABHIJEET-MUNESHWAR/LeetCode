199c3d3f85407c5daca5f71212bd4b99beb235cb
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 22:38:22 +0530

    Add Medium Java Solution for 452. Minimum Number of Arrows to Burst Balloons from LeetCode

---
+++ LeetCode/Patterns/Greedy/452. Minimum Number of Arrows to Burst Balloons/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        int arrowsCount = 1;
+        int size = points.length;
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int commonEndPoint = points[0][1];
+        for (int i = 1; i < size; i++) {
+            int currentStart = points[i][0];
+            int currentEnd = points[i][1];
+            if (commonEndPoint < currentStart) {
+                commonEndPoint = currentEnd;
+                arrowsCount++;
+            }
+        }
+        return arrowsCount;
+    }
+}
