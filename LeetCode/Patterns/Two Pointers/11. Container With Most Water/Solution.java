c106fbd896997d9ee5cac20d15d36cf9a46cc63d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 17:29:43 +0530

    Add Medium Java Solution for 11. Container With Most Water from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/11. Container With Most Water/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = Integer.MIN_VALUE;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
