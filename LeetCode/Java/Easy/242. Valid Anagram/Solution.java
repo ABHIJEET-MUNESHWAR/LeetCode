8859d8a8abb22f4080dbb07d13fe75d5c8a88816
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 22:38:50 +0530

    Add Easy Java Solution for 242. Valid Anagram from LeetCode

--- LeetCode/Java/Easy/242. Valid Anagram/Solution.java
+++ LeetCode/Java/Easy/242. Valid Anagram/Solution.java
@@ -4,21 +4,21 @@ class Solution {
         int len1 = s.length();
         int len2 = t.length();
         if (len1 != len2) {
-          return false;
+            return false;
         }
         int[] alphabet = new int[26];
         for (int i = 0; i < len1; i++) {
-          Character ch = s.charAt(i);
-          alphabet[ch - 'a']++;
+            Character ch = s.charAt(i);
+            alphabet[ch - 'a']++;
         }
         for (int i = 0; i < len1; i++) {
-          Character ch = t.charAt(i);
-          alphabet[ch - 'a']--;
+            Character ch = t.charAt(i);
+            alphabet[ch - 'a']--;
         }
         for (int i = 0; i < 26; i++) {
-          if (alphabet[i] != 0) {
-            return false;
-          }
+            if (alphabet[i] != 0) {
+                return false;
+            }
         }
         return isValidAnagram;
     }
