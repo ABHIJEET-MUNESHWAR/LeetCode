fe7226ca06be486f39e94481e8dd50d706c11731
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 21:57:21 +0530

    [Syncode] LeetCode/Easy/Happy Number

---
+++ LeetCode/Easy/Happy Number/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = getDigitSquareSum(slow);
+            fast = getDigitSquareSum(fast);
+            fast = getDigitSquareSum(fast);
+        } while (slow != fast);
+        if (slow == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int getDigitSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int mod = n % 10;
+            sum += mod * mod;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
