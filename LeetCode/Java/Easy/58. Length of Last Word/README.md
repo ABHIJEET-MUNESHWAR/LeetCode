a136c7c44118d8bfb8badc64e1e7eb6d47169765
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 16:21:38 +0530

    Add Easy Java Solution for 58. Length of Last Word from LeetCode

--- LeetCode/Java/Easy/58. Length of Last Word/README.md
+++ LeetCode/Java/Easy/58. Length of Last Word/README.md
@@ -8,8 +8,8 @@
 String
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 41.5 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Easy/58. Length of Last Word/Solution.java
+++ LeetCode/Java/Easy/58. Length of Last Word/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+        int n = s.length();
         int size = 0;
-        int len = s.length();
-        for (int i = len - 1; i >= 0; i--) {
-          if (s.charAt(i) != ' ') {
-            size++;
-          } else {
-            if (size > 0) {
-              return size;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                size++;
+            } else {
+                if (size > 0) {
+                    return size;
+                }
             }
-          }
         }
-        return size;      
+        return size;
     }
 }
