7e879abf57263a3ee142bdc8226bf940196b85c0
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 22:28:01 +0530

    Time: 7 ms (98.98%) | Memory: 48.9 MB (85.10%) - LeetSync

--- leetsync/56-merge-intervals/merge-intervals.java
+++ leetsync/56-merge-intervals/merge-intervals.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); // Sort based on start point
-        List<int[]> result = new ArrayList<>();
-        result.add(intervals[0]);
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> mergedIntervals = new ArrayList<>();
+        mergedIntervals.add(intervals[0]);
         int n = intervals.length;
         for (int i = 1; i < n; i++) {
-            int[] interval = intervals[i];
-            int currentStart = interval[0];
-            int currentEnd = interval[1];
-            if (currentStart > result.get(result.size() - 1)[1]) {
-                // No Overlap
-                result.add(interval);
+            int[] currentInterval = intervals[i];
+            int currentIntervalStart = currentInterval[0];
+            int currentIntervalEnd = currentInterval[1];
+            if (mergedIntervals.get(mergedIntervals.size() - 1)[1] < currentIntervalStart) {
+                // No overlap
+                mergedIntervals.add(currentInterval);
             } else {
-                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], currentEnd);
+                mergedIntervals.get(mergedIntervals.size() - 1)[1] = Math
+                        .max(mergedIntervals.get(mergedIntervals.size() - 1)[1], currentIntervalEnd);
             }
         }
-        return result.toArray(new int[result.size()][]);
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
     }
 }
