b6fee374c83326610e0cfe8de695ab0f8f128530
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 22:51:23 +0530

    Add Easy Java Solution for 20. Valid Parentheses from LeetCode

--- LeetCode/GRIND_75.md
+++ LeetCode/GRIND_75.md
@@ -3,7 +3,7 @@
 Track your progress on the Grind 75 coding interview preparation list.
 
 ## Progress
-- **Completed:** 25 / 75 (33.3%)
+- **Completed:** 26 / 75 (34.7%)
 
 ---
 
@@ -78,7 +78,7 @@ Track your progress on the Grind 75 coding interview preparation list.
 - [x] [Minimum Window Substring](./Java/Hard/76. Minimum Window Substring/)
 - [x] [Valid Anagram](./Java/Easy/242. Valid Anagram/)
 - [x] [Group Anagrams](./Java/Medium/49. Group Anagrams/)
-- [ ] Valid Parentheses
+- [x] [Valid Parentheses](./Java/Easy/20. Valid Parentheses/)
 - [x] [Valid Palindrome](./Java/Easy/125. Valid Palindrome/)
 - [ ] Longest Palindromic Substring
 - [ ] Palindromic Substrings
---
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isValid(String s) {
+        String expr = s;
+        int length = expr.length();
+        Stack stack = new Stack();
+        for (int i = 0; i < length; i++) {
+          Character c = expr.charAt(i);
+          Character peek = '0';
+          if(!stack.isEmpty()){
+            peek = (Character) stack.peek();
+          }
+          switch (c) {
+            case '(':
+              stack.push(c);
+              break;
+            case '[':
+              stack.push(c);
+              break;
+            case '{':
+              stack.push(c);
+              break;
+            case ')':
+              if (peek == '(') {
+                stack.pop();
+              } else {
+                stack.push(c);
+              }
+              break;
+            case ']':
+              if (peek == '[') {
+                stack.pop();
+              } else {
+                stack.push(c);
+              }
+              break;
+            case '}':
+              if (peek == '{') {
+                stack.pop();
+              } else {
+                stack.push(c);
+              }
+              break;
+          }
+        }
+        return stack.isEmpty();
+    }
+}
