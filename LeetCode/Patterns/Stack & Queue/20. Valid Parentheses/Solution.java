f250de3fdc3d5a90d698dfb914e09e4c20cefbe7
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 22:52:00 +0530

    Add Easy Java Solution for 20. Valid Parentheses from LeetCode

--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
@@ -4,43 +4,43 @@ class Solution {
         int length = expr.length();
         Stack stack = new Stack();
         for (int i = 0; i < length; i++) {
-          Character c = expr.charAt(i);
-          Character peek = '0';
-          if(!stack.isEmpty()){
-            peek = (Character) stack.peek();
-          }
-          switch (c) {
-            case '(':
-              stack.push(c);
-              break;
-            case '[':
-              stack.push(c);
-              break;
-            case '{':
-              stack.push(c);
-              break;
-            case ')':
-              if (peek == '(') {
-                stack.pop();
-              } else {
-                stack.push(c);
-              }
-              break;
-            case ']':
-              if (peek == '[') {
-                stack.pop();
-              } else {
-                stack.push(c);
-              }
-              break;
-            case '}':
-              if (peek == '{') {
-                stack.pop();
-              } else {
-                stack.push(c);
-              }
-              break;
-          }
+            Character c = expr.charAt(i);
+            Character peek = '0';
+            if (!stack.isEmpty()) {
+                peek = (Character) stack.peek();
+            }
+            switch (c) {
+                case '(':
+                    stack.push(c);
+                    break;
+                case '[':
+                    stack.push(c);
+                    break;
+                case '{':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (peek == '(') {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                    break;
+                case ']':
+                    if (peek == '[') {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                    break;
+                case '}':
+                    if (peek == '{') {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                    break;
+            }
         }
         return stack.isEmpty();
     }
