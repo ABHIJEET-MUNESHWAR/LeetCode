f5a8ae2bbbbdcf3c2e35eb7822a871098103cccc
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 15:27:55 +0530

    Add Medium Java Solution for 134. Gas Station from LeetCode

--- LeetCode/Patterns/Greedy/134. Gas Station/Solution.java
+++ LeetCode/Patterns/Greedy/134. Gas Station/Solution.java
@@ -1,24 +1,24 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-        int totalCost = 0;
-        int totalGas = 0;
+        int totalGas = 0, totalCost = 0;
         for (int i = 0; i < n; i++) {
-            totalCost += cost[i];
             totalGas += gas[i];
+            totalCost += cost[i];
         }
         if (totalGas < totalCost) {
             return -1;
         }
-        int result = 0;
-        int total = 0;
+        totalGas = 0;
+        int startIndex = 0;
         for (int i = 0; i < n; i++) {
-            total += gas[i] - cost[i];
-            if (total < 0) {
-                result = i + 1;
-                total = 0;
+            totalGas += gas[i] - cost[i];
+            startIndex = i;
+            if (totalGas < 0) {
+                startIndex = i + 1;
+                totalGas = 0;
             }
         }
-        return result;
+        return startIndex;
     }
 }
