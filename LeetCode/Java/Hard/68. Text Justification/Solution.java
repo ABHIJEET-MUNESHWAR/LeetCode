6cd8266b7e4d65a818578881bab51336accc0ed4
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 16:56:31 +0530

    Add Hard Java Solution for 68. Text Justification from LeetCode

---
+++ LeetCode/Java/Hard/68. Text Justification/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < words.length) {
+            int j = i, len = 0;
+            while (j < words.length && len + words[j].length() + (j - i) <= maxWidth) {
+                len += words[j].length();
+                j++;
+            }
+            int gaps = j - i - 1;
+            int spaces = maxWidth - len;
+            StringBuilder line = new StringBuilder();
+
+            if (j == words.length || gaps == 0) {
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+                    if (k != j - 1)
+                        line.append(" ");
+                }
+                while (line.length() < maxWidth)
+                    line.append(" ");
+            } else {
+                int spaceEach = spaces / gaps, extra = spaces % gaps;
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+                    if (k != j - 1) {
+                        int toAdd = spaceEach + (extra-- > 0 ? 1 : 0);
+                        line.append(" ".repeat(toAdd));
+                    }
+                }
+            }
+            result.add(line.toString());
+            i = j;
+        }
+        return result;
+    }
+}
