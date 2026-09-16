aed2ed9691e8ba9208fd919c2134282a6722ea73
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 21:33:48 +0530

    Add Easy Java Solution for 205. Isomorphic Strings from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/205. Isomorphic Strings/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/205. Isomorphic Strings/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-      int len = s.length();
-      Map<Character, Integer> m1 = new HashMap<>();
-      Map<Character, Integer> m2 = new HashMap<>();
-      for (Integer i = 0; i < len; i++) {
-        if (m1.put(s.charAt(i), i) != m2.put(t.charAt(i), i)) {
-          return false;
+        int len = s.length();
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
+        for (Integer i = 0; i < len; i++) {
+            if (m1.put(s.charAt(i), i) != m2.put(t.charAt(i), i)) {
+                return false;
+            }
         }
-      }
-      return true;
+        return true;
     }
 }
