b8bce1bc76da674ce7585ffe697f26df533e24e9
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-16 17:25:46 +0530

    Add Easy Java Solution for 392. Is Subsequence from LeetCode

--- LeetCode/Java/Easy/392. Is Subsequence/README.md
+++ LeetCode/Java/Easy/392. Is Subsequence/README.md
@@ -8,8 +8,8 @@
 Two Pointers, String, Dynamic Programming
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 40.3 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Dynamic Programming/392. Is Subsequence/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/392. Is Subsequence/Solution.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int index = -1;
         for (char c : s.toCharArray()) {
-          index = t.indexOf(c, index + 1);
-          if (index == -1) {
-            return false;
-          }
+            index = t.indexOf(c, index + 1);
+            if (index == -1) {
+                return false;
+            }
         }
         return true;
     }
