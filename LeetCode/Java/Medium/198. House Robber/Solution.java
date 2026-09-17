67fd555fdf1637ee601737af2c87120480d18c86
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-17 21:31:48 +0530

    Add Medium Java Solution for 198. House Robber from LeetCode

--- LeetCode/Java/Medium/198. House Robber/Solution.java
+++ LeetCode/Java/Medium/198. House Robber/Solution.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int robRecursion(int[] nums, int n, int index) {
+    public int robRecursion(int[] nums, int n, int index, int[] dp) {
         if (index >= n) {
             return 0;
         }
-        int take = nums[index] + robRecursion(nums, n, index + 2);
-        int skip = robRecursion(nums, n, index + 1);
-        return Math.max(take, skip);
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        int take = nums[index] + robRecursion(nums, n, index + 2, dp);
+        int skip = robRecursion(nums, n, index + 1, dp);
+        return dp[index] = Math.max(take, skip);
     }
 
     public int rob(int[] nums) {
         int n = nums.length;
-        return robRecursion(nums, n, 0);
+        int[] dp = new int[401];
+        Arrays.fill(dp, -1);
+        return robRecursion(nums, n, 0, dp);
     }
 }
