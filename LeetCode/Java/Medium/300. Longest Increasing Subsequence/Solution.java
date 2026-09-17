1d3d67521b02f187df7c24272fc18c7ef270f3cd
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-17 11:28:13 +0530

    Add Medium Java Solution for 300. Longest Increasing Subsequence from LeetCode

--- LeetCode/Java/Medium/300. Longest Increasing Subsequence/Solution.java
+++ LeetCode/Java/Medium/300. Longest Increasing Subsequence/Solution.java
@@ -1,28 +1,18 @@
 class Solution {
-    public int lengthOfLISRecursionMemoization(int[] nums, int n, int currentIndex, int previousIndex, int[][] dp) {
+    public int lengthOfLISRecursion(int[] nums, int n, int currentIndex, int previousIndex) {
         if (currentIndex >= n) {
             return 0;
         }
-        if (previousIndex != -1 && dp[currentIndex][previousIndex] != -1) {
-            return dp[currentIndex][previousIndex];
-        }
         int take = 0;
         if (previousIndex == -1 || nums[previousIndex] < nums[currentIndex]) {
-            take = 1 + lengthOfLISRecursionMemoization(nums, n, currentIndex + 1, currentIndex, dp);
+            take = 1 + lengthOfLISRecursion(nums, n, currentIndex + 1, currentIndex);
         }
-        int skip = lengthOfLISRecursionMemoization(nums, n, currentIndex + 1, previousIndex, dp);
-        if (previousIndex != -1) {
-            dp[currentIndex][previousIndex] = Math.max(take, skip);
-        }
+        int skip = lengthOfLISRecursion(nums, n, currentIndex + 1, previousIndex);
         return Math.max(take, skip);
     }
 
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return lengthOfLISRecursionMemoization(nums, n, 0, -1, dp);
+        return lengthOfLISRecursion(nums, n, 0, -1);
     }
 }
