49e4be80bd0fb1d5c7803359ad6e07666e13df4f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-17 11:29:42 +0530

    Add Medium Java Solution for 139. Word Break from LeetCode

--- LeetCode/Java/Medium/139. Word Break/Solution.java
+++ LeetCode/Java/Medium/139. Word Break/Solution.java
@@ -2,28 +2,22 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet<>();
         wordDictSet.addAll(wordDict);
-        Boolean[] dp = new Boolean[301];
-        return wordBreakBackTrack(s, wordDictSet, s.length(), 0, dp);
+        return wordBreakBackTrack(s, wordDictSet, s.length(), 0);
     }
 
-    public boolean wordBreakBackTrack(String s, Set<String> wordDictSet, int n, int i, Boolean[] dp) {
+    public boolean wordBreakBackTrack(String s, Set<String> wordDictSet, int n, int i) {
         if (i >= n) {
             return true;
         }
         if (wordDictSet.contains(s)) {
             return true;
         }
-        if (dp[i] != null) {
-            return dp[i];
-        }
         for (int j = i + 1; j <= n; j++) {
             String subString = s.substring(i, j);
-            if (wordDictSet.contains(subString) && wordBreakBackTrack(s, wordDictSet, n, j, dp)) {
-                dp[i] = true;
+            if (wordDictSet.contains(subString) && wordBreakBackTrack(s, wordDictSet, n, j)) {
                 return true;
             }
         }
-        dp[i] = false;
         return false;
     }
 }
