5e172875b95527d8b26b756337fa928c3df0fe00
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-18 15:23:47 +0530

    Add Hard Java Solution for 460. LFU Cache from LeetCode

--- LeetCode/Java/Hard/460. LFU Cache/Solution.java
+++ LeetCode/Java/Hard/460. LFU Cache/Solution.java
@@ -1,55 +1,49 @@
 class LFUCache {
-
-    private int capacity;
-    private int minFreq = 0;
-    private Map<Integer, Integer> keyToVal = new HashMap<>();
-    private Map<Integer, Integer> keyToFreq = new HashMap<>();
-    private Map<Integer, LinkedHashSet<Integer>> freqToLRUKeys = new HashMap<>();
+    int minFreq = 0;
+    int capacity = 0;
+    Map<Integer, Integer> keyToValueMap = new HashMap<>();
+    Map<Integer, Integer> keyToFreqMap = new HashMap<>();
+    Map<Integer, LinkedHashSet<Integer>> freqToLRUKeys = new HashMap<>();
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        if (!keyToVal.containsKey(key))
+        if (!keyToValueMap.containsKey(key)) {
             return -1;
-
-        final int freq = keyToFreq.get(key);
+        }
+        int freq = keyToFreqMap.get(key);
         freqToLRUKeys.get(freq).remove(key);
         if (freq == minFreq && freqToLRUKeys.get(freq).isEmpty()) {
             freqToLRUKeys.remove(freq);
-            ++minFreq;
+            minFreq++;
         }
-
-        // Increase key's freq by 1
-        // Add this key to next freq's list
         putFreq(key, freq + 1);
-        return keyToVal.get(key);
+        return keyToValueMap.get(key);
     }
 
     public void put(int key, int value) {
-        if (capacity == 0)
-            return;
-        if (keyToVal.containsKey(key)) {
-            keyToVal.put(key, value);
-            get(key); // Update key's count
+        if (capacity == 0) {
             return;
         }
-
-        if (keyToVal.size() == capacity) {
-            // Evict LRU key from the minFreq list
-            final int keyToEvict = freqToLRUKeys.get(minFreq).iterator().next();
+        if (keyToValueMap.containsKey(key)) {
+            keyToValueMap.put(key, value);
+            get(key);
+            return;
+        }
+        if (keyToValueMap.size() == capacity) {
+            int keyToEvict = freqToLRUKeys.get(minFreq).iterator().next();
             freqToLRUKeys.get(minFreq).remove(keyToEvict);
-            keyToVal.remove(keyToEvict);
+            keyToValueMap.remove(keyToEvict);
         }
-
         minFreq = 1;
-        putFreq(key, minFreq); // Add new key and freq
-        keyToVal.put(key, value); // Add new key and value
+        keyToValueMap.put(key, value);
+        putFreq(key, minFreq);
     }
 
-    private void putFreq(int key, int freq) {
-        keyToFreq.put(key, freq);
+    public void putFreq(int key, int freq) {
+        keyToFreqMap.put(key, freq);
         freqToLRUKeys.putIfAbsent(freq, new LinkedHashSet<>());
         freqToLRUKeys.get(freq).add(key);
     }
