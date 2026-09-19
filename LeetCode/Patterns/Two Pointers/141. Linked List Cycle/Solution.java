911ca2bd62084d51aced2f6b885df92a439a3a63
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-19 15:58:43 +0530

    Add Easy Java Solution for 141. Linked List Cycle from LeetCode

--- LeetCode/Patterns/Two Pointers/141. Linked List Cycle/Solution.java
+++ LeetCode/Patterns/Two Pointers/141. Linked List Cycle/Solution.java
@@ -11,14 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slowPointer = head;
-        ListNode fastPointer = head;
-        while (slowPointer != null && fastPointer != null && fastPointer.next != null) {
-            slowPointer = slowPointer.next;
-            fastPointer = fastPointer.next.next;
-            if (slowPointer == fastPointer) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == null) {
+            return false;
+        }
+        while (slow.next != null && fast.next.next != null) {
+            if (slow.val == fast.val) {
                 return true;
             }
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return false;
     }
