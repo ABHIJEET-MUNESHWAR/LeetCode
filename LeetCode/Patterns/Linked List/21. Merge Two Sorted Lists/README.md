eb21e980f89aaa206836cfa9511a7ddabff92231
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-19 19:38:03 +0530

    Add Easy Java Solution for 21. Merge Two Sorted Lists from LeetCode

--- LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/README.md
+++ LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/README.md
@@ -8,8 +8,8 @@
 Linked List, Recursion
 
 ### 🚀 Performance
-- **Runtime:** 1 ms
-- **Memory:** 43.1 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/Solution.java
+++ LeetCode/Patterns/Linked List/21. Merge Two Sorted Lists/Solution.java
@@ -9,19 +9,19 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {        
-        if (head1 == null) {
-          return head2;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
         }
-        if (head2 == null) {
-          return head1;
+        if (list2 == null) {
+            return list1;
         }
-        if (head1.val < head2.val) {
-          head1.next = mergeTwoLists(head1.next, head2);
-          return head1;
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         } else {
-          head2.next = mergeTwoLists(head1, head2.next);
-          return head2;
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
     }
 }
