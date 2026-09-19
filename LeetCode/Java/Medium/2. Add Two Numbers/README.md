9c283b177455eace4ebb7524b8b1a05dc748b93c
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-09-19 16:34:03 +0530

    Add Medium Java Solution for 2. Add Two Numbers from LeetCode

--- LeetCode/Java/Medium/2. Add Two Numbers/README.md
+++ LeetCode/Java/Medium/2. Add Two Numbers/README.md
@@ -8,8 +8,8 @@
 Linked List, Math, Recursion
 
 ### 🚀 Performance
-- **Runtime:** 1 ms
-- **Memory:** 44.4 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Linked List/2. Add Two Numbers/Solution.java
+++ LeetCode/Patterns/Linked List/2. Add Two Numbers/Solution.java
@@ -1,50 +1,35 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode list1, ListNode list2) {
-
-        if (list1 == null) {
-            return list2;
-        }
-        if (list2 == null) {
-            return list1;
-        }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode();
+        ListNode current = l3;
         int carry = 0;
-        ListNode list3 = new ListNode(-1);
-        ListNode head = list3;
-        while (list1 != null && list2 != null) {
-            int sum = list1.val + list2.val + carry;
+        while (l1 != null || l2 != null || carry != 0) {
+            int value1 = l1 == null ? 0 : l1.val;
+            int value2 = l2 == null ? 0 : l2.val;
+            int sum = carry + value1 + value2;
+            int digit = sum % 10;
             carry = sum / 10;
-            list3.next = new ListNode(sum % 10);
-            list3 = list3.next;
-            list1 = list1.next;
-            list2 = list2.next;
+            ListNode nextNode = new ListNode(digit);
+            current.next = nextNode;
+            current = current.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+
         }
-        while (list1 != null) {
-            int sum = list1.val + carry;
-            carry = sum / 10;
-            list3.next = new ListNode(sum % 10);
-            list3 = list3.next;
-            list1 = list1.next;
-        }
-        while (list2 != null) {
-            int sum = list2.val + carry;
-            carry = sum / 10;
-            list3.next = new ListNode(sum % 10);
-            list3 = list3.next;
-            list2 = list2.next;
-        }
-        if (carry > 0) {
-            list3.next = new ListNode(carry);
-        }
-        return head.next;
+        return l3.next;
     }
 }
